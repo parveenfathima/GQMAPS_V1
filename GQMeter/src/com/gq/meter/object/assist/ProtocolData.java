23cf8f1dde6ecd2194863aea552f6d77f68d5d5b
mistersriram <mistersriram@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2012-11-26 04:25:29 +0000

    new assist package

---
+++ GQMeter/src/com/gq/meter/object/assist/ProtocolData.java
@@ -0,0 +1,26 @@
+package com.gq.meter.object.assist;
+
+public class ProtocolData {
+
+    // name of the protocol implemented.. can only be from a list of implemented protocols such as
+    // air , water , router , computer , power , switch , printer etc
+    String protocol;
+
+    // contains the actual protocol data , like power , computer , water etc
+    String data;
+
+    public ProtocolData(String protocol, String data) {
+        super();
+        this.protocol = protocol;
+        this.data = data;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+}
