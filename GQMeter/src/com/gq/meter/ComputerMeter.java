44b6e00f6c96fc1472122c9b0abc2a6264b8735a
yogalakshmi <yogalakshmi@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2012-11-26 05:37:35 +0000

    Removed unused imports for computer meter

--- GQMeter/src/com/gq/meter/ComputerMeter.java
+++ GQMeter/src/com/gq/meter/ComputerMeter.java
@@ -8,21 +8,11 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
-import java.util.Vector;
 import java.util.concurrent.TimeUnit;
 
 import org.snmp4j.CommunityTarget;
-import org.snmp4j.PDU;
 import org.snmp4j.Snmp;
-import org.snmp4j.Target;
-import org.snmp4j.TransportMapping;
-import org.snmp4j.event.ResponseEvent;
-import org.snmp4j.mp.SnmpConstants;
-import org.snmp4j.smi.Integer32;
-import org.snmp4j.smi.Null;
 import org.snmp4j.smi.OID;
-import org.snmp4j.smi.OctetString;
-import org.snmp4j.smi.UdpAddress;
 import org.snmp4j.smi.VariableBinding;
 import org.snmp4j.transport.DefaultUdpTransportMapping;
 
