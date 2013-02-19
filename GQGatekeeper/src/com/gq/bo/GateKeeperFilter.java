69cd5feadadcaeb3d074daaccf3b58fff62f57d7
mistersriram <mistersriram@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-02-19 07:21:41 +0000

    initial version

---
+++ GQGatekeeper/src/com/gq/bo/GateKeeperFilter.java
@@ -0,0 +1,18 @@
+package com.gq.bo;
+
+import com.gq.meter.GQMeterResponse;
+
+// this class is takes care of validating and distributing incoming requests
+
+public class GateKeeperFilter {
+	
+	public void process(GQMeterResponse gqmResponse) {
+		
+		//parse the object and get the protocols and save them for now..... ss , feb 19 , 2013
+		System.out.println("ready to parse and save....");
+		
+		
+	}
+	
+}
+
---
+++ GQGatekeeper/src/com/gq/restsvcs/Test.java
@@ -0,0 +1,51 @@
+package com.gq.restsvcs;
+
+
+import java.net.URI;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+
+public class Test {
+	public static void main(String[] args) {
+		ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config);
+		WebResource service = client.resource(getBaseURI());
+		// Fluent interfaces
+		System.out.println(service.path("gqgk").path("gatekeeper").accept(
+				MediaType.TEXT_PLAIN).get(ClientResponse.class).toString());
+		System.out.println(service.path("gqgk").path("hello").accept(
+				MediaType.TEXT_PLAIN).get(String.class).toString());
+
+		System.out.println("-------text plain over--------------\n");
+		
+		// Get app xml text
+		System.out.println(service.path("rest").path("gatekeeper").accept(
+				MediaType.APPLICATION_XML).get(String.class));
+		
+		System.out.println("-------- app xml over -------------\n");
+
+		// Get XML
+		System.out.println(service.path("rest").path("hello").accept(
+				MediaType.TEXT_XML).get(String.class));
+		System.out.println("------text xml over--------------\n");
+
+		// The HTML
+		System.out.println(service.path("rest").path("hello").accept(
+				MediaType.TEXT_HTML).get(String.class));
+	}
+
+	private static URI getBaseURI() {
+		return UriBuilder.fromUri(
+				"http://localhost:8080/RESTProject/").build();
+	}
+
+}
+
+		
