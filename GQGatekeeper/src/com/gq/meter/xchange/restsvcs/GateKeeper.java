d27cc08d5020c556e886325f1e256a107ce50596
pchandru <pchandru@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-03-05 08:56:21 +0000

    Changed GQGatekeeper as GQEnterprseDataProcessor, so major code has been removed and package names have been changed.

---
+++ GQGatekeeper/src/com/gq/meter/xchange/restsvcs/GateKeeper.java
@@ -0,0 +1,63 @@
+package com.gq.meter.xchange.restsvcs;
+
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.gq.meter.GQMeterResponse;
+import com.gq.meter.xchange.filter.GateKeeperFilter;
+
+//import com.sun.jerse
+// POJO, no interface no extends
+
+// The class registers its methods for the HTTP GET request using the @GET annotation. 
+// Using the @Produces annotation, it defines that it can deliver several MIME types,
+// text, XML and HTML. 
+
+// The browser requests per default the HTML MIME type.
+
+//Sets the path to base URL + /hello
+@Path("/gatekeeper")
+public class GateKeeper {
+
+    // This method is called if TEXT_PLAIN is request
+    @GET
+    @Produces({ MediaType.TEXT_PLAIN, MediaType.TEXT_XML, MediaType.APPLICATION_XML })
+    public String sayHello() {
+        return "Gate keeper is up and running.....";
+    }
+
+    // This method is called if HTML is request
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String sayHtmlHello() {
+        return "<html> " + "<title>" + "Hello Jersey text html" + "</title>" + "<body><h1>" + "Hello Jersey text html "
+                + "</body></h1>" + "</html> ";
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void keepGate(@FormParam("gqMeterResponse") String gqMeterResponseString) throws IOException {
+
+        System.out.println("Incoming json is => " + gqMeterResponseString);
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        GQMeterResponse gqMeterResponse = gson.fromJson(gqMeterResponseString, GQMeterResponse.class);
+        // unmarshall the response from the gqmeter running in premise and start processing.....
+
+        GateKeeperFilter gkf = new GateKeeperFilter();
+
+        gkf.process(gqMeterResponse);
+    }
+
+}
