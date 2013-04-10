39c47ba2cff78bd05faa4b71dd8e3c507510765b
pchandru <pchandru@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-04-10 09:23:57 +0000

    Initial checin of GQmapsRegistrationservices project

---
+++ trunk/GQMapsRegistrationServices/src/com/gq/meter/model/EnterpriseModel.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.gq.meter.model;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import com.gq.meter.object.Enterprise;
+import com.gq.meter.util.GQGateKeeperConstants;
+import com.gq.meter.util.HibernateUtil;
+
+/**
+ * @author Chandru
+ * 
+ */
+public class EnterpriseModel {
+
+    public List<Enterprise> getAllEnterprises() {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
+
+            String hql = "FROM Enterprise";
+            Query query = session.createQuery(hql);
+            List<Enterprise> entMeterResult = query.list();
+            return entMeterResult;
+        }
+        catch (Exception e) {
+            GQGateKeeperConstants.logger.error("Exception occured while fetching the enterprises ", e);
+            return null;
+        }
+        finally {
+            try {
+                if (session.isOpen()) {
+                    session.flush();
+                    session.close();
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
