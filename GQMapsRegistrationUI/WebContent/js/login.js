da8c9badc249b8cf60c0e3f6a5747ee92e7b8429
hema <hema@itechins.com>
2013-04-17 11:46:31 +0000

    completed the basic scripting required for adding enterprise and ready for ajax call once receive the rest service for post method

--- GQMapsRegistrationUI/WebContent/js/login.js
+++ GQMapsRegistrationUI/WebContent/js/login.js
@@ -1,59 +1,114 @@
-// JavaScript Document
-
 //binding the click event to the submit button
-$(document).ready(function() {
+$(document).ready(function() 
+{
 	$("#submit").bind("click", userLogin);
 });
 
-// login validation for both admin and customer
-function userLogin() {
+// login validation for both admin and customers
+function userLogin() 
+{
 	var user = $.trim($('#txtUserId').val());
 	var pwd = $.trim($('#pwdPassword').val());
 	var isValid = 1;
+	
+	$.jStorage.set("jsUserId", "");
+	$.jStorage.set("jsPwd", "");
 
-	if (user.length === 0) {
+	if (user.length === 0) 
+	{
 		alert("Enter Username");
 		$('#txtUserId').focus();
 		isValid = 0;
-	} else if (pwd.length === 0) {
+	} else if (pwd.length === 0) 
+	{
 		alert("Enter Password");
 		$('#pwdPassword').focus();
 		isValid = 0;
-	} else if ((user == 'admin') && (pwd == 'admin')) {
+	} else if ((user == 'admin') && (pwd == 'admin')) 
+	{
 		window.location.href = "edit_registration.html";
 		$.jStorage.set("jsUserID", user);
 	} else {
 
 		var vUrl = 'http://localhost:8080/GQMapsRegistrationServices/gqm-gk/gatekeeper/getregistration';
 
-		alert("before ajax call");
 		$.ajax({
 			type : "GET",
 			url : vUrl,
 			dataType : "json",
-			success : function(json) {
-				alert("Record length is :" + json.length);
+			success : function(json) 
+			{
+				var vRecLen = json.length;
+				//alert("Record length is :" + vRecLen);
 				//alert(JSON.stringify(json));
+				$('#productInfo').append(json);
 				
-			},
-			error : function(json) {
+				if(vRecLen != 0)
+				{
+					$('#productInfo').append(json[0]["userId"] + " " + json[0]["passwd"] +"\n" + json[1]["userId"] + " " + json[1]["passwd"] +"\n" 
+					+ json[2]["userId"] + " " + json[2]["passwd"]);
+					
+					if(user === "admin" && pwd ==="admin")
+					{
+						isValid = 1;
+						$.jStorage.set("jsUserId", user);
+						$.jStorage.set("jsPwd", pwd);						
+						window.location.href = "edit_registration.html";
+					}
+					else
+					{
+						$.each(json, function(i,n)
+						{											
+							if( user === $.trim(n["userId"]) && pwd === $.trim(n["passwd"]))
+							{
+								isValid = 1;
+								$.jStorage.set("jsUserId", user);
+								$.jStorage.set("jsPwd", pwd);								
+								window.location.href = "add_registration.html";						
+							}
+							else
+							{
+								isValid = 0;
+							}
+						  
+						});
+						
+						if(isValid === 0)
+						{
+							alert("Invalid User");	
+							$.jStorage.set("jsUserId", "");
+							$.jStorage.set("jsPwd", "");	
+							$('#txtUserId').val("");
+							$('#pwdPassword').val("");	
+							$('#txtUserId').focus();												
+						}
+					}					
+				}
+				
+			}, //end of success
+			error : function(json) 
+			{
 				alert("Error: " + json.status + " " + json.responseText);
 			}
-
 		});
 	}
 }
 
-function checkForValue() {
+//change password validation. User id input is must to navigate to the change_password page
+function checkForValue() 
+{
 	var user = $.trim($('#txtUserId').val());
 
-	if (user.length == 0) {
+	if (user.length === 0) 
+	{
 		alert("Enter your user id to change password");
+			
 		window.location.href = "login.html"
 		$("#txtUserId").focus();
-	} else {
+	} 
+	else 
+	{
 		$.jStorage.set("jsUserID", user);
-
 		window.location.href = "change_password.html";
 	}
 }
