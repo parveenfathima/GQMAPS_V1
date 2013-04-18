2ed0c950a63b0541e64d5b8f844a5cf47bf4cfe4
pchandru <pchandru@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-04-18 11:12:30 +0000

    checking in rest services code for fetching all the enterprise and saving new new enterprises

--- trunk/GQMapsRegistrationServices/src/com/gq/meter/restsvcs/GateKeeperServices.java
+++ trunk/GQMapsRegistrationServices/src/com/gq/meter/restsvcs/GateKeeperServices.java
@@ -2,11 +2,16 @@ package com.gq.meter.restsvcs;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.gq.meter.model.EnterpriseModel;
 import com.gq.meter.object.Enterprise;
 import com.gq.meter.util.GQGateKeeperConstants;
@@ -15,33 +20,41 @@ import com.gq.meter.util.GQGateKeeperConstants;
  * @author Chandru
  * 
  */
-@Path("/gatekeeper")
-
+@Path("/enterprise")
 public class GateKeeperServices {
+    private Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
 
     /**
      * This method used to fetch all the enterprises
      * 
      * @return
      */
-    @GET
     @Path("/getregistration")
-    @Produces("application/json")
-       
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public String getAllEnterprises() {
-    	
-		String feedsEnterprise  = null;
-		
         GQGateKeeperConstants.logger.info("Generating all the enterprises list from GQGatekeeper");
         EnterpriseModel entmodel = new EnterpriseModel();
         List<Enterprise> entMeterResult = entmodel.getAllEnterprises();
-
-        //Converting the object json format using the gson object.
-        Gson gson = new Gson();
-        feedsEnterprise = gson.toJson(entMeterResult);
-        
-        //System.out.println(entMeterResult);
-        return feedsEnterprise;
+        // Returning all the enterprises in JSON format
+        return gson.toJson(entMeterResult);
     }
 
+    @Path("/addregistration")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addEnterprise(String entObjectString) {
+        Enterprise entObject = null;
+        try {
+            entObject = gson.fromJson(entObjectString, Enterprise.class);
+            GQGateKeeperConstants.logger.info("Saving the new enterprise : " + entObject.getEnterpriseId());
+            EnterpriseModel entmodel = new EnterpriseModel();
+            entmodel.createEnterprise(entObject);
+        }
+        catch (Exception e) {
+            GQGateKeeperConstants.logger.error("Exception occured while creating the new enterprise", e);
+            return Response.status(400).build();
+        }
+        return Response.ok(gson.toJson(entObjectString)).build();
+    }
 }
