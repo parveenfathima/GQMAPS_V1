3c5c7a274a7478f46886818d833f75849c6f2944
hema@itechins.com <hema@itechins.com@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-04-19 11:34:44 +0000

    updated edit registration with the basic requirements. Used GET method of Enterprise.

--- trunk/GQMapsRegistrationUI/WebContent/js/edit_registration.js
+++ trunk/GQMapsRegistrationUI/WebContent/js/edit_registration.js
@@ -1,136 +1,278 @@
-// JavaScript Document
 
-//datepicker 
+var arrEntp = new Array(enterprise()); // global array to store all the enterprises from the rest service
+var gEId = ""; // global enterprise id.
+var gRegStatus = "";
+
 $(document).ready(function() 
+{
+	$( "#txtNewExpiry").bind("focusin", showDate);
+});
+
+// saving the enterprise general information dialog
+function saveGeneral()
+{
+	updateEntp(arrEntp[gEId]);   
+}
+
+// updating only the changed object
+function updateEntp(dbEntp)
+{
+		var formEntp = new enterprise($.trim($("#txtEID").val()), $.trim($("#txtEName").val()), $.trim($("#txtUID").val()), $.trim($("#txtPwd").val()), $.trim($("#cmbSaveFwd").val()), $.trim($("#txtUrl").val()), dbEntp.getRegStatus());
+		
+		if(compareObject(dbEntp, formEntp))
+		{
+			alert("No changes");
+		}
+		else
+		{
+			alert("Changes found");
+			
+			//TODO call to ajax
+			
+			// gEId = ""
+		}
+}
+
+// compare the enterprise values before and after the change. If changed, then only the ajax will be called to update.
+function compareObject(obj1, obj2)
+{
+	for(var parameters in obj1)
+	{
+		if(obj1[parameters] !== obj2[parameters])
+		{
+			return false;
+		}
+	}
+	
+	for(var parameters in obj2)
+	{
+		if(obj1[parameters] !== obj2[parameters]){
+			return false;
+		}
+	}
+	
+	return true;
+}
+
+
+// binding the datepicker with the field
+function showDate()
 {
 	$("#txtNewExpiry").datepicker({
 		minDate: 0
 	});
+}
+
+// opening the general dialog with the selected enterprise's basic information to edit by the admin user. 
+function openGeneralDialog(i)
+{
+	gEId = i;
+	$( "#dlgGeneral" ).dialog( "open" );
+	$("#hBasic").text(arrEntp[i].getEId() + " - " + arrEntp[i].getEName());
+	$("#txtEID").val(arrEntp[i].getEId());
+	$("#txtEName").val(arrEntp[i].getEName());
+	$("#txtUID").val(arrEntp[i].getUId());
+	$("#txtPwd").val(arrEntp[i].getPwd());
+	$("#cmbSaveFwd").val(arrEntp[i].getOutput());
+	$("#txtUrl").val(arrEntp[i].getUrl());
 	
-	 $("#accordion").hide();
+	$("#txtEID").focus();
 	
+}
+
+// opening the meter dialog for the selected enterprise to add the meter details. 
+function openMeterDialog(eId)
+{
+	$( "#dlgMeter" ).dialog( "open" );
+	$("#txtMeterID").text(eId);
+}
+
+// opening the meter dialog for the selected enterprise to add the validity details. 
+function openValidityDialog(eId)
+{
+	$( "#dlgValidity" ).dialog( "open" );
+	$('#txtNewExpiry').blur(); 
+}
+
+//General form dialog configuration
+$(function() {
+	  $( "#dlgGeneral" ).dialog({
+		  autoOpen: false,
+		  height: 435,
+		  width: 300,
+		  modal: true,
+		  position: "center"
+	  });		  
 });
 
-function buttonFormatter(row, cell, value, columnDef, dataContext)
-{  
-	var button = "<input type='button' class='btnEdit' id='create-user' value = 'Edit' />";
-    return button;
-}
-
-function callbackmethod(row) 
-{
-//	alert(data[row].eid);
-	alert(data[row].ename);
-}
-
-
-function btnMeterFormatter(row, cell, value, columnDef, dataContext)
-{  
-	var button = "<input type='button' class='btnMeter' id='btnMeter' value = 'Meter' onclick='javascript:callbackmethod2("+row+");' />";
-    return button;
-}
-
-function callbackmethod2(row) 
-{
-//	alert(data[row].eid);
-	alert(data[row].ename);
-}
-
-function btnValidityFormatter(row, cell, value, columnDef, dataContext)
-{  
-	var button = "<input type='button' class='btnValidity' id='btnValidity' value = 'Validity' onclick='javascript:callbackmethod3("+row+");' />";
-    return button;
-}
-
-function callbackmethod3(row) 
-{
-//	alert(data[row].eid);
-	alert(data[row].ename);
-	window.location.href = "login.html";
-}
-
-//dynamic grid for listing the enterprises
-var gridNewEntList, gridRegEntList;
-var data = [];
-var columns = [
-  {id: "action", name: "Action", field: "action", formatter: buttonFormatter, width:50},
-  {id: "meter", name: "Meter", field: "meter", formatter: btnMeterFormatter, width:70},
-  {id: "validity", name: "Validity", field: "validity", formatter: btnValidityFormatter, width:70},  
-  {id: "eid", name: "EID", field: "eid"},
-  {id: "ename", name: "EName", field: "ename"},
-  {id: "uid", name: "UID", field: "uid"},
-  {id: "pwd", name: "Password", field: "pwd"}
-
-];
-var countCol = 0;
-var options = {
-  enableCellNavigation: true,
-  enableColumnReorder: false
-};
-
-$(function () {
-
-  for (var i = 0; i < 10; i++) {
-	data[i] = {
-	  action: "button",
-	  meter: "button",
-	  validity: "button",
-	  eid: "E" + i,
-	  ename: "NE02/04/2013",
-	  uid: "U" + i,
-	  pwd: "********"
-	};
-  }
-  
-  var dataView = new Slick.Data.DataView();
-  
-  gridNewEntList = new Slick.Grid("#grdNewEList", data, columns, options);
-  gridRegEntList = new Slick.Grid("#grdRegEList", data, columns, options);
-   
+//Meter form dialog configuration
+$(function() {
+	  $( "#dlgMeter" ).dialog({
+		  autoOpen: false,
+		  height: 502,
+		  width: 300,
+		  modal: true,
+		  position: "center"
+	  });		  
 });
 
-
-///dynamic grid for listing the meters of the selected enterprise
-
-var gridMeterList;
-var meterColumns = [
-  {id: "mid", name: "Meter ID", field: "mid"},
-  {id: "protocol", name: "Protocol", field: "protocol"},
-  {id: "phone", name: "Phone", field: "phone"},
-  {id: "address", name: "Address", field: "address"},
-  {id: "description", name: "Description", field: "description", width: 230}
-];
-
-$(function () {
-  var meterData = [];
-  for (var i = 0; i < 1; i++) 
-  {
-	meterData[i] = {
-	  mid: "1",
-	  protocol: "Computer",
-	  phone: "22222222",
-	  address: "Chennai",
-	  description: "Purchase of computer meter"
-	};
-  }
-  gridMeterList = new Slick.Grid("#grdMeterList", meterData, meterColumns, options);
- 
-})
-
-//entAccordion
-
-
-
-
-
-//dialog
-
-/* $(function() {
-$( "#dialog-form" ).dialog();
-}); */
-
-
-$( "#submit" ).button().click(function() {
-$( "#dialog-form" ).dialog( "open" );
+//Validity form dialog configuration
+$(function() {
+	  $( "#dlgValidity" ).dialog({
+		  autoOpen: false,
+		  height: 365,
+		  width: 300,
+		  modal: true,
+		  position: "center"
+	  });
 });
+
+// dynamic grid listing all the enterprises
+function listEnterprise()
+{			
+		var vUrl = 'http://localhost:8080/GQMapsRegistrationServices/gqm-gk/enterprise/getregistration';
 		
+		$.ajax({
+			type : "GET",
+			beforeSend: function(xhr) 
+			{ 
+			  xhr.setRequestHeader("Content-Type", "application/json");
+			},			
+			url : vUrl,
+			dataType : "json",
+			success : function(json) 
+			{			
+				var vRecLen = json.length;
+				
+				if(vRecLen != 0) // listing the enterprise
+				{					
+						var vEntpList = "";	
+						var vEId = "";	
+
+						$.each(json, function(i,n)
+						{		
+							var vEId = n["enterpriseId"];
+							gRegStatus = n["regCmplt"];
+							
+							arrEntp[i] = new enterprise(n["enterpriseId"], n["EName"], n["userId"], n["passwd"], n["storeFwd"], n["fwdUrl"], n["regCmplt"]);											
+							
+							if(gRegStatus === 'n')
+								vEntpList += '<tr style="height:25px; color: #FF0000">';
+							else
+								vEntpList += '<tr>';
+								
+							vEntpList += '<td> <input type = "button" class = "button-font-a" id = "btnGeneral" onClick = "openGeneralDialog(' + i + ')"/></td>';
+							vEntpList += '<td> <input type = "button" class = "button-font-a" id = "btnMeter" onClick = "openMeterDialog(' + i + ')"/></td>';
+							vEntpList += '<td> <input type = "button" class = "button-font-a" id = "btnValidity" onClick = "openValidityDialog(' + i + ')"/></td>';                                            
+							vEntpList += '<td class = "td-font-b">' +  arrEntp[i].getEId() + '</td>';
+							vEntpList += '<td class = "td-font-b">' +  arrEntp[i].getEName() + '</td>';
+							vEntpList += '<td class = "td-font-b">' +  arrEntp[i].getUId()  + '</td>';
+							vEntpList += '<td class = "td-font-b">' +  arrEntp[i].getPwd() + '</td>';
+							vEntpList += '<td class = "td-font-b">' +  arrEntp[i].getRegStatus() + '</td>';
+							vEntpList += '</tr>';														
+						});	
+						
+						$("#tblEList").append(vEntpList);		
+									
+				} // end of if(vRecLen != 0 && vUserID != "admin")
+				else
+				{
+						alert("No records found");	
+						$.jStorage.set("jsUserId", "");	
+						window.location.href = "login.html";	
+				} // end of if(vRecLen != 0)
+				
+			}, //end of success
+			error : function(json) 
+			{
+				alert("Error: " + json.status + " " + json.responseText);
+			}
+		});			  		
+}
+
+// enterprise object's constructor, get and set methods		
+function enterprise(eid, ename, uid, pwd, output, url, regStatus)   
+{
+	this.eid = eid;
+	this.ename = ename;
+	this.uid = uid;
+	this.pwd = pwd;
+	this.output = output;
+	this.url = url;
+	this.regStatus = regStatus;
+}
+
+enterprise.prototype.getEId = function()
+{
+	return this.eid;	
+}
+
+enterprise.prototype.setEId = function(eid)
+{
+	this.eid = eid;
+}
+
+enterprise.prototype.getEName = function()
+{
+	return this.ename;
+}
+
+enterprise.prototype.setEName = function(ename)
+{
+	this.ename = ename;
+}
+
+enterprise.prototype.getUId = function()
+{
+	return this.uid;
+}
+
+enterprise.prototype.setUId = function(uid)
+{
+	this.uid = uid;
+}
+
+enterprise.prototype.getPwd = function()
+{
+	return this.pwd;
+}
+
+enterprise.prototype.setPwd = function(pwd)
+{
+	this.pwd = pwd;
+}
+
+enterprise.prototype.getOutput = function()
+{
+	return this.output;
+}
+
+enterprise.prototype.setOutput = function(output)
+{
+	this.output = output;
+}
+
+enterprise.prototype.getUrl = function()
+{
+	return this.url;
+}
+
+enterprise.prototype.setUrl = function(url)
+{
+	this.url = url;
+}
+
+enterprise.prototype.getRegStatus = function()
+{
+	return this.regStatus;
+}
+
+enterprise.prototype.setRegStatus = function(url)
+{
+	this.url = regStatus;
+}
+
+enterprise.prototype.toString = function()  
+{
+	return(this.eid + " " + this.ename + " " + this.uid + " " + this.pwd + " " + this.output + " " + this.url + " " + this.regStatus);
+} // end of subject object	
