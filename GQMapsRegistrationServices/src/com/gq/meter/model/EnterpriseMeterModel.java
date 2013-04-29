b9db74572c023091b9cccfc22ed71a1b56d2cb06
pchandru <pchandru@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-04-29 09:09:17 +0000

    RESTful services for enterprisemeter

---
+++ GQMapsRegistrationServices/src/com/gq/meter/model/EnterpriseMeterModel.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package com.gq.meter.model;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import com.gq.meter.object.EnterpriseMeter;
+import com.gq.meter.util.GQGateKeeperConstants;
+import com.gq.meter.util.HibernateUtil;
+
+/**
+ * @author GQ
+ * 
+ */
+public class EnterpriseMeterModel {
+
+    /**
+     * This method used to fetch all the enterprises
+     * 
+     * @return
+     */
+    public List<EnterpriseMeter> getAllEnterpriseMeters() throws Exception {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
+
+            String hql = "FROM EnterpriseMeter";
+            Query query = session.createQuery(hql);
+            List<EnterpriseMeter> entMeterResult = query.list();
+            return entMeterResult;
+        }
+        catch (Exception e) {
+            GQGateKeeperConstants.logger.error("Exception occured while fetching the enterprises ", e);
+            throw new Exception(e);
+        }
+        finally {
+            try {
+                if (session.isOpen()) {
+                    session.flush();
+                    session.close();
+                }
+            }
+            catch (Exception e) {
+                throw new Exception(e);
+            }
+        }
+    }
+
+    /**
+     * This method is used to add new enterprise meter
+     * 
+     * @param entMeterObject
+     * @throws Exception
+     */
+    public void addEnterpriseMeter(EnterpriseMeter entMeterObject) throws Exception {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
+            session.save(entMeterObject);
+            session.getTransaction().commit();
+        }
+        catch (Exception e) {
+            GQGateKeeperConstants.logger.error("Exception occured while creating the enterprise ", e);
+            throw new Exception(e);
+        }
+        finally {
+            try {
+                if (session.isOpen()) {
+                    session.flush();
+                    session.close();
+                }
+            }
+            catch (Exception e) {
+                throw new Exception(e);
+            }
+        }
+    }
+
+}
---
+++ GQMapsRegistrationServices/src/com/gq/meter/restsvcs/EnterpriseMeterServices.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package com.gq.meter.restsvcs;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.gq.meter.model.EnterpriseMeterModel;
+import com.gq.meter.model.EnterpriseModel;
+import com.gq.meter.object.Enterprise;
+import com.gq.meter.object.EnterpriseMeter;
+import com.gq.meter.util.GQGateKeeperConstants;
+import com.gq.meter.util.GQRegistrationConstants;
+
+/**
+ * @author Chandru
+ * 
+ */
+@Path("/enterpriseMeters")
+public class EnterpriseMeterServices {
+
+    /**
+     * This method used to fetch all the enterprises
+     * 
+     * @return
+     */
+    @Path("/getEntMeters")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllEnterprises() {
+        GQGateKeeperConstants.logger.info("Generating all the enterprise meters list from GQGatekeeper");
+        EnterpriseMeterModel entMeterModel = new EnterpriseMeterModel();
+        List<EnterpriseMeter> entMeterResult = null;
+        try {
+            entMeterResult = entMeterModel.getAllEnterpriseMeters();
+        }
+        catch (Exception e) {
+            GQGateKeeperConstants.logger.error("Exception occured while fetching the enterprises list ", e);
+            return Response.status(400).build();
+        }
+        // Returning all the enterprises in JSON format
+        return Response.ok(GQRegistrationConstants.gson.toJson(entMeterResult)).build();
+    }
+
+    @Path("/addEntMeters")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addEnterpriseMeter(String entMeterString) {
+        EnterpriseMeter entMeterObject = null;
+        try {
+            entMeterObject = GQRegistrationConstants.gson.fromJson(entMeterString, EnterpriseMeter.class);
+            GQGateKeeperConstants.logger.info("Saving the new enterprise : " + entMeterObject.getEnterpriseId());
+            EnterpriseMeterModel entMeterModel = new EnterpriseMeterModel();
+            entMeterModel.addEnterpriseMeter(entMeterObject);
+        }
+        catch (Exception e) {
+            GQGateKeeperConstants.logger.error("Exception occured while creating the new enterprise", e);
+            return Response.status(400).build();
+        }
+        return Response.ok("success").build();
+    }
+
+}
