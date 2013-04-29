cb3cfa1bb88cfa925fe78b519dc51d3eabd4fa04
pchandru <pchandru@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-04-29 11:02:52 +0000

    RESTful services for protocol

---
+++ trunk/GQMapsRegistrationServices/src/com/gq/meter/model/ProtocolModel.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.gq.meter.model;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import com.gq.meter.object.Protocol;
+import com.gq.meter.util.GQGateKeeperConstants;
+import com.gq.meter.util.HibernateUtil;
+
+/**
+ * @author GQ
+ * 
+ */
+public class ProtocolModel {
+
+    public List<Protocol> getAllProtocols() throws Exception {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
+
+            String hql = "FROM Protocol";
+            Query query = session.createQuery(hql);
+            List<Protocol> protoResult = query.list();
+            return protoResult;
+        }
+        catch (Exception e) {
+            GQGateKeeperConstants.logger.error("Exception occured while fetching the enterprises ", e);
+            throw new Exception(e);
+        }
+        finally {
+            try {
+                if (session.isOpen()) {
+                    session.flush();
+                    session.close();
+                }
+            }
+            catch (Exception e) {
+                throw new Exception(e);
+            }
+        }
+    }
+
+}
---
+++ trunk/GQMapsRegistrationServices/src/com/gq/meter/restsvcs/ProtocolServices.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.gq.meter.restsvcs;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.gq.meter.model.ProtocolModel;
+import com.gq.meter.object.Protocol;
+import com.gq.meter.util.GQGateKeeperConstants;
+import com.gq.meter.util.GQRegistrationConstants;
+
+/**
+ * @author GQ
+ * 
+ */
+@Path("/protocol")
+public class ProtocolServices {
+
+    /**
+     * This method used to fetch all the protocols
+     * 
+     * @return
+     */
+    @Path("/getProtocols")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllEnterprises() {
+        GQGateKeeperConstants.logger.info("Generating all the enterprise meters list from GQGatekeeper");
+        ProtocolModel protoModel = new ProtocolModel();
+        List<Protocol> protocolResult = null;
+        try {
+            protocolResult = protoModel.getAllProtocols();
+        }
+        catch (Exception e) {
+            GQGateKeeperConstants.logger.error("Exception occured while fetching the enterprises list ", e);
+            return Response.status(400).build();
+        }
+        // Returning all the enterprises in JSON format
+        return Response.ok(GQRegistrationConstants.gson.toJson(protocolResult)).build();
+    }
+
+}
