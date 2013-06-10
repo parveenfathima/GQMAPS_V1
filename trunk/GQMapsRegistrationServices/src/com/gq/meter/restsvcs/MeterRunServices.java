a0e999a540287c3545553d73146563d4c1d69987
hema@itechins.com <hema@itechins.com@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-06-10 12:07:27 +0000

    added new service for finding the last scan

--- trunk/GQMapsRegistrationServices/src/com/gq/meter/restsvcs/MeterRunServices.java
+++ trunk/GQMapsRegistrationServices/src/com/gq/meter/restsvcs/MeterRunServices.java
@@ -10,36 +10,65 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.gq.meter.model.MeterRunModel;
+import com.gq.meter.object.EntpMeterRun;
 import com.gq.meter.object.MeterRun;
 import com.gq.meter.util.GQGateKeeperConstants;
 import com.gq.meter.util.GQRegistrationConstants;
+
 /**
  * @author Rathish
  * 
  */
 
 @Path("/meterrun")
+public class MeterRunServices
+{
 
-public class MeterRunServices {
-    
-    @Path("/getMeterRun")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getMeterRun(@QueryParam("entpId") String entpId) {
-        
-        GQGateKeeperConstants.logger.info("Generating all the enterprise meters list from EnterpriseMeter");
-        MeterRunModel meterModel = new MeterRunModel();
-        List<MeterRun> meterResult = null;
-        
-        try {
-            meterResult = meterModel.getMeterRun(entpId);
-        }
-        catch (Exception e) {
-            GQGateKeeperConstants.logger.error("Exception occured while fetching the meter list ", e);
-            return Response.status(400).build();
-        }
-        // Returning all the meterList in JSON format
-        return Response.ok(GQRegistrationConstants.gson.toJson(meterResult)).build();
-    }
+	@Path("/getMeterRun")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getMeterRun(@QueryParam("entpId") String entpId)
+	{
+
+		GQGateKeeperConstants.logger.info("Generating all the enterprise meters list from EnterpriseMeter");
+		MeterRunModel meterModel = new MeterRunModel();
+		List<MeterRun> meterResult = null;
+
+		try
+		{
+			meterResult = meterModel.getMeterRun(entpId);
+		}
+		catch (Exception e)
+		{
+			GQGateKeeperConstants.logger.error("Exception occured while fetching the meter list ", e);
+			return Response.status(400).build();
+		}
+		// Returning all the meterList in JSON format
+		return Response.ok(GQRegistrationConstants.gson.toJson(meterResult)).build();
+	}
+
+	@Path("/getLastScan")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getLastScan(@QueryParam("entpId") String entpId)
+	{
+
+		GQGateKeeperConstants.logger.info("Retrieving the last scan");
+		System.out.println("inside service");
+		MeterRunModel meterRunModel = new MeterRunModel();
+		List<EntpMeterRun> entpMeterRun = null;
+
+		try
+		{
+			entpMeterRun = meterRunModel.getLastScan(entpId);
+		}
+		catch (Exception e)
+		{
+			GQGateKeeperConstants.logger.error("Exception occured while fetching the meter list ", e);
+			return Response.status(400).build();
+		}
+		// Returning all the meterList in JSON format
+		return Response.ok(GQRegistrationConstants.gson.toJson(entpMeterRun)).build();
+	}
 
 }
