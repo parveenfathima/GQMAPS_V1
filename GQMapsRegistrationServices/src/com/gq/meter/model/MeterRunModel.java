a231517eed15053c1118eda631d37465c0a8bf5f
hema <hema@itechins.com>
2013-06-10 12:06:31 +0000

    added new service for finding the last scan

--- GQMapsRegistrationServices/src/com/gq/meter/model/MeterRunModel.java
+++ GQMapsRegistrationServices/src/com/gq/meter/model/MeterRunModel.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
+import com.gq.meter.object.EntpMeterRun;
 import com.gq.meter.object.MeterRun;
 import com.gq.meter.util.GQGateKeeperConstants;
 import com.gq.meter.util.HibernateUtil;
@@ -17,34 +18,83 @@ import com.gq.meter.util.HibernateUtil;
  * @param <MeterRun>
  * 
  */
-public class MeterRunModel {
+public class MeterRunModel
+{
 
-    public List<MeterRun> getMeterRun(String entpId) throws Exception {
-        Session session = null;
-        try {
-            session = HibernateUtil.getSessionFactory().getCurrentSession();
-            session.beginTransaction();
-            String hql = "SELECT COUNT(meterId) FROM EnterpriseMeter WHERE enterpriseId = :ENT_ID";
-            Query query = session.createQuery(hql);
-            query.setParameter("ENT_ID", entpId);
-            List<MeterRun> meterResult = query.list();
-            return meterResult;
-        }
-        catch (Exception e) {
-            GQGateKeeperConstants.logger.error("Exception occured while fetching the number of meters ", e);
-            throw new Exception(e);
-        }
-        finally {
-            try {
-                if (session.isOpen()) {
-                    session.flush();
-                    session.close();
-                }
-            }
-            catch (Exception e) {
-                GQGateKeeperConstants.logger.error("Exception occured while closing the session ", e);
-                throw new Exception(e);
-            }
-        }
-    }
+	public List<MeterRun> getMeterRun(String entpId) throws Exception
+	{
+		Session session = null;
+		try
+		{
+			session = HibernateUtil.getSessionFactory().getCurrentSession();
+			session.beginTransaction();
+			String hql = "SELECT COUNT(meterId) FROM EnterpriseMeter WHERE enterpriseId = :ENT_ID";
+			Query query = session.createQuery(hql);
+			query.setParameter("ENT_ID", entpId);
+			List<MeterRun> meterResult = query.list();
+			return meterResult;
+		}
+		catch (Exception e)
+		{
+			GQGateKeeperConstants.logger.error("Exception occured while fetching the number of meters ", e);
+			throw new Exception(e);
+		}
+		finally
+		{
+			try
+			{
+				if (session.isOpen())
+				{
+					session.flush();
+					session.close();
+				}
+			}
+			catch (Exception e)
+			{
+				GQGateKeeperConstants.logger.error("Exception occured while closing the session ", e);
+				throw new Exception(e);
+			}
+		}
+	}
+
+	public List<EntpMeterRun> getLastScan(String entpId) throws Exception
+	{
+		Session session = null;
+		try
+		{
+			session = HibernateUtil.getSessionFactory().getCurrentSession();
+			session.beginTransaction();
+			//String hql = "SELECT MAX(recDttm), assetScnd, assetDisc FROM meter_run WHERE enterpriseId = :ENT_ID";
+			
+			String hql = "select b.enterpriseId, MAX(a.recDttm), a.assetScnd, a.assetDisc from MeterRun a, EnterpriseMeter b where a.meterId = b.meterId and b.enterpriseId = :ENT_ID";
+			
+			
+			Query query = session.createQuery(hql);
+			query.setParameter("ENT_ID", entpId);
+			List<EntpMeterRun> meterRunResult = query.list();
+			System.out.println("resultant record count is :" + meterRunResult.size());
+			return meterRunResult;
+		}
+		catch (Exception e)
+		{
+			GQGateKeeperConstants.logger.error("Exception occured while fetching the last scan: ", e);
+			throw new Exception(e);
+		}
+		finally
+		{
+			try
+			{
+				if (session.isOpen())
+				{
+					session.flush();
+					session.close();
+				}
+			}
+			catch (Exception e)
+			{
+				GQGateKeeperConstants.logger.error("Exception occured while closing the session for last scan service ", e);
+				throw new Exception(e);
+			}
+		}
+	}
 }
