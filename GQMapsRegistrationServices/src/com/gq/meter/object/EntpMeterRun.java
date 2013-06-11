eae1e7f7b19244d8f88b1d9957bde924889f78b8
hema <hema@itechins.com>
2013-06-11 05:16:32 +0000

    format done

--- GQMapsRegistrationServices/src/com/gq/meter/object/EntpMeterRun.java
+++ GQMapsRegistrationServices/src/com/gq/meter/object/EntpMeterRun.java
@@ -2,20 +2,24 @@ package com.gq.meter.object;
 
 public class EntpMeterRun
 {
-	private MeterRun meterRun;
-	private EnterpriseMeter entpMeter;
+	private MeterRun		meterRun;
+	private EnterpriseMeter	entpMeter;
+
 	public MeterRun getMeterRun()
 	{
 		return meterRun;
 	}
+
 	public void setMeterRun(MeterRun meterRun)
 	{
 		this.meterRun = meterRun;
 	}
+
 	public EnterpriseMeter getEntpMeter()
 	{
 		return entpMeter;
 	}
+
 	public void setEntpMeter(EnterpriseMeter entpMeter)
 	{
 		this.entpMeter = entpMeter;
