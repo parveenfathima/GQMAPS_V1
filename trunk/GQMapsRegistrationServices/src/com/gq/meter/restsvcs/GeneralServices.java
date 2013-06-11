691a288e5cc9961ed1b1040e0a71a9440f7f5c1f
rathish <rathish@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-06-11 14:25:07 +0000

    Completed enterprise summary service for all the enterprises in editRegistration

--- trunk/GQMapsRegistrationServices/src/com/gq/meter/restsvcs/GeneralServices.java
+++ trunk/GQMapsRegistrationServices/src/com/gq/meter/restsvcs/GeneralServices.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -18,27 +19,46 @@ import com.gq.meter.util.GQGateKeeperConstants;
 import com.gq.meter.util.GQRegistrationConstants;
 
 /**
- * @author GQ
+ * @author Rathish
  * 
  */
 @Path("/general")
 public class GeneralServices {
-    @Path("/getEntpSummary")
+    @Path("/getEntpList")
     @GET
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response getEntpSummary(@QueryParam("entpId") String entpId) {
+    public Response getEntpList(@QueryParam("entpId") String entpId) {
 
-        GQGateKeeperConstants.logger.info("authString : " + entpId);
+        GQGateKeeperConstants.logger.info("EnterpriseId is for single enterprise details is: : " + entpId);
         EntpSummaryModel entpModel = new EntpSummaryModel();
         List<EntpSummary> result = null;
         try {
-            result = entpModel.getEntpSummary(entpId);
+            result = entpModel.getEntpList(entpId);
         }
         catch (Exception e) {
             GQGateKeeperConstants.logger.error("Exception occured while fetching the details of enterpriseid ", e);
             return Response.status(400).build();
         }
         return Response.ok(GQRegistrationConstants.gson.toJson(result)).build();
+        // return Response.ok(200).build();
+    }
+
+    @Path("/getEntpSummary")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getEntpSummary() {
+
+        GQGateKeeperConstants.logger.info("Generating summary of all enterprises");
+        EntpSummaryModel entpModel = new EntpSummaryModel();
+        List<EntpSummary> summaryResult = null;
+        try {
+            summaryResult = entpModel.getEntpSummary();
+        }
+        catch (Exception e) {
+            GQGateKeeperConstants.logger.error("Exception occured while fetching the details of enterpriseid ", e);
+            return Response.status(400).build();
+        }
+        return Response.ok(GQRegistrationConstants.gson.toJson(summaryResult)).build();
         // return Response.ok(200).build();
     }
 }
