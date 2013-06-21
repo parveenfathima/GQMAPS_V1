33b69203e32b431bef613bafaaee887d0b2f023e
rathish <rathish@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-06-21 10:59:17 +0000

    Updated log file path for GQEnterprisedata

--- trunk/GQEntrprseDataProcessor/src/com/gq/util/GQEDPConstants.java
+++ trunk/GQEntrprseDataProcessor/src/com/gq/util/GQEDPConstants.java
@@ -17,7 +17,7 @@ public class GQEDPConstants {
     public static final Logger logger = Logger.getLogger("GQEDPConstants.class");
 
     private static FileAppender fileappender;
-    private static String logFilePrefix = "/opt/gq/maps/logs";
+    private static String logFilePrefix = "/opt/gq/maps/logs/";
     private static String logFileName = "enterpriseDataProcessor.log";
 
     private static final String LOG_FILE_NAME_DATE_PATTERN = "yyyy-MM-dd";
@@ -26,7 +26,7 @@ public class GQEDPConstants {
         logger.info("Inside create logger method");
 
         GQEDPConstants.logger.info("initializing gqmaps EntrprseDataProcessor logger..."); // ???? !!!!!!
-        String logFile = logFilePrefix + "\\" + logFileName;
+        String logFile = logFilePrefix + logFileName;
         try {
             fileappender = new org.apache.log4j.DailyRollingFileAppender(new PatternLayout(
                     "  %-15C{1} | %d{MMM dd HH:mm} | %5p | %m%n"), logFile, LOG_FILE_NAME_DATE_PATTERN);
