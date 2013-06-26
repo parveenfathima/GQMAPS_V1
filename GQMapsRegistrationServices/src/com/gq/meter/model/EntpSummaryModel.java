2030f41458dcc3653b4f79226af9410bc1379bcb
hema <hema@itechins.com>
2013-06-26 07:45:14 +0000

    getting key value pair in json format for multiple table queries

--- GQMapsRegistrationServices/src/com/gq/meter/model/EntpSummaryModel.java
+++ GQMapsRegistrationServices/src/com/gq/meter/model/EntpSummaryModel.java
@@ -3,6 +3,7 @@
  */
 package com.gq.meter.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Query;
@@ -24,7 +25,7 @@ public class EntpSummaryModel {
         try {
             session = HibernateUtil.getSessionFactory().getCurrentSession();
             session.beginTransaction();
-            String hql = "select a.enterpriseId, a.eName, count(b.meterId), c.expDttm from Enterprise a, EnterpriseMeter b, GateKeeper c  where a.enterpriseId = b.enterpriseId AND a.enterpriseId = c.enterpriseId AND a.enterpriseId=:ENT_ID";
+            String hql = "select a.sid, a.enterpriseId, a.eName, count(b.meterId), c.expDttm from Enterprise a, EnterpriseMeter b, GateKeeper c  where a.enterpriseId = b.enterpriseId AND a.enterpriseId = c.enterpriseId AND a.enterpriseId=:ENT_ID";
             Query query = session.createQuery(hql);
             query.setParameter("ENT_ID", entpId);
             List<EntpSummary> result = query.list();
@@ -53,12 +54,26 @@ public class EntpSummaryModel {
         try {
             session = HibernateUtil.getSessionFactory().getCurrentSession();
             session.beginTransaction();
-            // String hql =
-            // "select a.enterpriseId, a.eName, count(b.meterId), c.expDttm from Enterprise a, EnterpriseMeter b, GateKeeper c  where a.enterpriseId = b.enterpriseId AND a.enterpriseId = c.enterpriseId group by a.enterpriseId";
-            String hql = "select a.enterpriseId, a.blCd, a.eName, a.phone, a.email, a.userId, a.passwd, a.secQtn1, a.ans1, a.secQtn2, a.ans2, a.storeFwd, a.fwdUrl, a.noOfEmpl, a.entSqft, a.entAssetCount, a.dcSqft, a.dcAssetCount, a.dcUsePctg, a.dcTemp, a.regCmplt, a.active, a.comments, a.creDttm, count(b.meterId), c.expDttm from Enterprise a, EnterpriseMeter b, GateKeeper c  where a.enterpriseId = b.enterpriseId AND a.enterpriseId = c.enterpriseId group by a.enterpriseId";
+            String hql = "select a.sid, a.enterpriseId, a.blCd, a.eName, a.phone, a.email, a.userId, a.passwd, a.secQtn1, a.ans1, a.secQtn2, a.ans2, a.storeFwd, a.fwdUrl, a.noOfEmpl, a.entSqft, a.entAssetCount, a.dcSqft, a.dcAssetCount, a.dcUsePctg, a.dcTemp, a.regCmplt, a.active, a.comments, a.creDttm, count(b.meterId), c.expDttm from Enterprise a, EnterpriseMeter b, GateKeeper c  where a.enterpriseId = b.enterpriseId AND a.enterpriseId = c.enterpriseId group by a.enterpriseId";
+            
             Query query = session.createQuery(hql);
-            List<EntpSummary> summaryResult = query.list();
-            return summaryResult;
+            List<Object[]> summaryResult = query.list();
+            System.out.println(summaryResult);
+            
+            List<EntpSummary> entpSummaryList = new ArrayList<EntpSummary>();
+            
+            for(Object[] list: summaryResult){
+            	EntpSummary entpSummary = new EntpSummary();
+            	entpSummary.setSid((Short) list[0]);            	
+            	entpSummary.setEnterpriseId((String) list[1]);
+            	//TODO: set all the fields
+            	
+
+            	entpSummaryList.add(entpSummary);
+            }
+			
+			
+            return entpSummaryList;
         }
         catch (Exception e) {
             GQGateKeeperConstants.logger.error("Exception occured while fetching the enterpriseid ", e);
@@ -66,7 +81,8 @@ public class EntpSummaryModel {
         }
         finally {
             try {
-                if (session.isOpen()) {
+                if (session.isOpen()) 
+                {
                     session.flush();
                     session.close();
                 }
