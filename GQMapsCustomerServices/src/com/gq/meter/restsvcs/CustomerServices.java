9d3d113c0f7b287b0885d11a714e3b7d74d4c49e
rathish <rathish@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-07-04 09:38:47 +0000

    New service for retrieving Asset count and updated hibernate config file

--- GQMapsCustomerServices/src/com/gq/meter/restsvcs/CustomerServices.java
+++ GQMapsCustomerServices/src/com/gq/meter/restsvcs/CustomerServices.java
@@ -1,12 +1,16 @@
 package com.gq.meter.restsvcs;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.gq.meter.model.AssetModel;
 import com.gq.meter.model.CustomerServiceModel;
 import com.gq.meter.object.AllCustomerServices;
+import com.gq.meter.object.Asset;
 import com.gq.meter.util.CustomerServiceConstant;
 
 @Path("/customerservices")
@@ -28,4 +32,15 @@ public class CustomerServices {
         return CustomerServiceConstant.gson.toJson(customerServiceResult);
     }
 
+    @Path("/getAssetCount")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAssetCount() {
+        CustomerServiceConstant.logger.info("Generating all the customer service list from GQEntrprseDataProcessor");
+        AssetModel asset = new AssetModel();
+        List<Asset> assetresult = asset.getAssetCount();
+        // Returning all the enterprises in JSON format
+        CustomerServiceConstant.logger.info("Number of asset Id in asset" + assetresult);
+        return CustomerServiceConstant.gson.toJson(assetresult);
+    }
 }
