222635ac48335ba3c26e3d5dc446e9a3b3aeb8b2
parveenf <parveenf@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-08-07 06:31:06 +0000

    Added new Constant files for GQMapsCustomerServices

---
+++ GQMapsCustomerServices/src/com/gq/meter/util/GetAssetServiceConstant.java
@@ -0,0 +1,44 @@
+package com.gq.meter.util;
+
+import java.io.IOException;
+import java.util.Date;
+
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class GetAssetServiceConstant {
+    // logger specifics
+    public static final Logger logger = Logger.getLogger("GetAssetServiceConstant.class");
+
+    private static FileAppender fileappender;
+    private static String logFilePrefix = "/opt/gq/maps/logs/";
+    private static String logFileName = "getassetservices.log";
+
+    private static final String LOG_FILE_NAME_DATE_PATTERN = "yyyy-MM-dd";
+
+    public static final Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+
+    static {
+        logger.info("Inside create logger method");
+
+        GetAssetServiceConstant.logger.info("initializing gqmaps GetAssetServices logger..."); // ???? !!!!!!
+        String logFile = logFilePrefix + logFileName;
+        try {
+            fileappender = new org.apache.log4j.DailyRollingFileAppender(new PatternLayout(
+                    "  %-15C{1} | %d{MMM dd HH:mm} | %5p | %m%n"), logFile, LOG_FILE_NAME_DATE_PATTERN);
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            GetAssetServiceConstant.logger.error("************ Unable to create LOG file handle for file  " + logFile
+                    + " at " + new Date() + ". TERMINATING server ??.... ************"); // ???? !!!!!!
+        }
+
+        logger.addAppender(fileappender);
+        logger.info("after create logger method");
+    }
+}
