1507e2ba33afcd8290cdbc7dab77f0dc0f05dc72
rathish <rathish@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-08-13 14:06:54 +0000

    working charts in progress

--- GQMapsCustomerServices/src/com/gq/meter/restsvcs/DashboardServices.java
+++ GQMapsCustomerServices/src/com/gq/meter/restsvcs/DashboardServices.java
@@ -28,6 +28,7 @@ import com.google.visualization.datasource.datatable.value.ValueType;
 import com.google.visualization.datasource.render.JsonRenderer;
 import com.gq.meter.object.Data;
 import com.gq.meter.object.TaskAssist;
+import com.gq.meter.util.CustomerServiceConstant;
 import com.ibm.icu.util.GregorianCalendar;
 import com.ibm.icu.util.TimeZone;
 import com.mysql.jdbc.PreparedStatement;
@@ -67,9 +68,11 @@ public class DashboardServices {
         String str = null;
         double val = 0;
         Date dt = null;
-        String chartData = null;
+        String chartData = "";
+        double plainData = 0;
+        String lineData = "";
         TaskAssist taskAssist = new TaskAssist(taskId, descr, sql, dynamic, chartType, columnHeader, relatedDb,
-                positionId, data, chartData);
+                positionId, data, chartData, plainData, lineData);
         PreparedStatement pstmt = null;
         String alist = "";
         String result = "";
@@ -77,8 +80,6 @@ public class DashboardServices {
         List<TaskAssist> sqllist = new ArrayList<TaskAssist>();
         DataTable linedata = new DataTable();
         DataTable bardata = new DataTable();
-        GregorianCalendar calendar = new GregorianCalendar();
-        calendar.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         CharSequence renderchart = null;
         TaskAssist jsonArrayList;
@@ -88,24 +89,25 @@ public class DashboardServices {
             Class.forName("com.mysql.jdbc.Driver");
             dbCon = DriverManager.getConnection(dbURL, username, password);
             dbCon1 = DriverManager.getConnection(dbURL1, username, password);
+            CustomerServiceConstant.logger.info("Executing the ");
             String currentSql = "select * from task_asst WHERE pos_id IS NOT NULL;";
             // getting PreparedStatment to execute query
             stmt = (Statement) dbCon.prepareStatement(currentSql);
             // Resultset returned by query
             rs = stmt.executeQuery(currentSql);
             while (rs.next()) {
-                List<Data> datalist = new ArrayList<Data>();
+                JSONArray chartArray = new JSONArray();
 
-                System.out.println("inside  while");
+                // System.out.println("inside  while");
                 taskId = rs.getInt("ts_id");
                 descr = rs.getString("descr");
                 sql = rs.getString("tsql");
                 dynamic = rs.getString("dynamic");
                 // String ctId = rs.getString("ct_id");
                 String chartquery = "select ct_Id,descr from chart_type where ct_id=?";
-                System.out.println("ct-id");
+                // System.out.println("ct-id");
                 taskAssist.setChartType(rs.getString("ct_id"));
-                System.out.println("ct-id" + taskAssist.setChartType(rs.getString("ct_id")));
+                // System.out.println("ct-id" + taskAssist.setChartType(rs.getString("ct_id")));
                 // stmt = (Statement) dbCon.prepareStatement(chartquery);
                 pstmt = (PreparedStatement) dbCon.prepareStatement(chartquery);
                 pstmt.setString(1, taskAssist.setChartType(rs.getString("ct_id")));
@@ -116,7 +118,7 @@ public class DashboardServices {
                     System.out.println("charttype\t" + chartType + "descr\t" + descr1);
                 }
                 String[] colHeader = null;
-                System.out.println("col header: " + rs.getString("col_hdr"));
+                // System.out.println("col header: " + rs.getString("col_hdr"));
                 if (rs.getString("col_hdr") != null && rs.getString("col_hdr").trim() != "") {
 
                     if ((rs.getString("col_hdr").split(",").length) >= 2) {
@@ -129,8 +131,8 @@ public class DashboardServices {
                 }
                 relatedDb = rs.getString("relatd_db");
                 positionId = rs.getString("pos_id");
-                System.out.println("tsid\t" + taskId + "descr" + descr + "tsql" + sql + "colheader" + columnHeader
-                        + "posid" + positionId);
+                // System.out.println("tsid\t" + taskId + "descr" + descr + "tsql" + sql + "colheader" + columnHeader
+                // + "posid" + positionId);
                 TaskAssist obj = new TaskAssist();
 
                 obj.setTaskId(taskId);
@@ -152,16 +154,16 @@ public class DashboardServices {
                 // sqllist.add(obj);
 
                 if (relatedDb.equalsIgnoreCase("m")) {
-                    System.out.println("maps::::::::::");
+                    // System.out.println("maps::::::::::");
 
                     String entpquery = sql;
-                    System.out.println("query is \t" + entpquery);
+                    // System.out.println("query is \t" + entpquery);
                     stmt = (Statement) dbCon1.prepareStatement(entpquery);
                     // Resultset returned by query
                     entpResultset = stmt.executeQuery(entpquery);
                 }
                 else {
-                    System.out.println("inside else");
+                    // System.out.println("inside else");
                     String entpquery = sql;
                     stmt = (Statement) dbCon.prepareStatement(entpquery);
                     // Resultset returned by query
@@ -171,12 +173,35 @@ public class DashboardServices {
                 // find col count c =2;
                 ResultSetMetaData rsmd = entpResultset.getMetaData();
                 int count = rsmd.getColumnCount();
+
+                // System.out.println("cout value is ::" + count);
+                DataTable chartdata = new DataTable();
+
+                if (chartType.equals("bar") || chartType.equals("pie")) {
+                    ArrayList<ColumnDescription> colum = new ArrayList<ColumnDescription>();
+
+                    // System.out.println("inside bar or  pie");
+
+                    colum.add(new ColumnDescription(colHeader[0], ValueType.TEXT, colHeader[0]));
+                    colum.add(new ColumnDescription(colHeader[1], ValueType.NUMBER, colHeader[1]));
+                    // System.out.println("clo1" + colHeader[0] + "col2" + colHeader[1]);
+                    chartdata.addColumns(colum);
+                }
+                else if (chartType.equals("line")) {
+                    ArrayList<ColumnDescription> colum = new ArrayList<ColumnDescription>();
+
+                    // System.out.println("inside line");
+                    // DataTable chartdata = new DataTable();
+
+                    colum.add(new ColumnDescription(colHeader[0], ValueType.DATETIME, colHeader[0]));
+                    colum.add(new ColumnDescription(colHeader[1], ValueType.NUMBER, colHeader[1]));
+                    chartdata.addColumns(colum);
+                }
                 List<Data> cDataList = new ArrayList<Data>();
-                System.out.println("cout value is ::" + count);
+
                 while (entpResultset.next()) {
 
-                    System.out.println("result \t");
-                    Data datavalue = new Data();
+                    // System.out.println("result \t");
                     Data cData = new Data();
                     for (int i = 1; i <= count; i++) {
 
@@ -185,80 +210,89 @@ public class DashboardServices {
                         if (type == Types.VARCHAR || type == Types.CHAR) {
                             // str = datavalue.setName(entpResultset.getString(i));
                             cData.setName(entpResultset.getString(i));
-                            System.out.println("inside if" + cData.setName(entpResultset.getString(i)));
+                            // System.out.println("inside if" + cData.setName(entpResultset.getString(i)));
                         }
+                        // else if (type == Types.DATE) {
                         else if (type == Types.TIMESTAMP) {
                             // dt = datavalue.setDate(entpResultset.getTimestamp(i));
-                            cData.setDate(entpResultset.getTimestamp(i));
-                            System.out.println("inside else if date" + dt);
+                            // cData.setDate(entpResultset.getTimestamp(i));
+                            cData.setName(entpResultset.getString(i));
+                            System.out.println("inside else if date" + cData.getName());
                         }
                         else {
                             // val = datavalue.setValue(entpResultset.getDouble(i));
                             cData.setValue(entpResultset.getDouble(i));
-                            System.out.println("inside else" + cData.setValue(entpResultset.getDouble(i)));
+                            // System.out.println("inside else" + cData.setValue(entpResultset.getDouble(i)));
                         }
 
                     }
+
                     cDataList.add(cData);
-                    // chart typr data convertion to be added<to-do>
-                    DataTable chartdata = new DataTable();
+                }
 
-                    if (chartType.equals("bar") || chartType.equals("pie")) {
-                        ArrayList<ColumnDescription> colum = new ArrayList<ColumnDescription>();
-
-                        System.out.println("inside bar or  pie");
-
-                        colum.add(new ColumnDescription(colHeader[0], ValueType.TEXT, colHeader[0]));
-                        colum.add(new ColumnDescription(colHeader[1], ValueType.NUMBER, colHeader[1]));
-                        System.out.println("clo1" + colHeader[0] + "col2" + colHeader[1]);
-                        chartdata.addColumns(colum);
-                        for (int i = 0; i < cDataList.size(); i++) {
-                            chartdata.addRowFromValues(cDataList.get(i).getName(), cDataList.get(i).getValue());
-
-                            System.out.println("\ndata1\t" + cDataList.get(i).getName() + "\tcdata2\t"
-                                    + cDataList.get(i).getValue());
-                        }
-                        // json.put("chart data", JsonRenderer.renderDataTable(chartdata, true, true));
+                // chart typr data convertion to be added<to-do>
+                if (chartType.equals("bar") || chartType.equals("pie")) {
+                    for (int i = 0; i < cDataList.size(); i++) {
+                        chartdata.addRowFromValues(cDataList.get(i).getName(), cDataList.get(i).getValue());
                     }
-                    else if (chartType.equals("line")) {
-                        ArrayList<ColumnDescription> colum = new ArrayList<ColumnDescription>();
+                    // System.out.println("\ndata1\t" + cDataList.get(i).getName() + "\tcdata2\t"
+                    // + cDataList.get(i).getValue());
+                }
+                // json.put("chart data", JsonRenderer.renderDataTable(chartdata, true, true));
 
-                        System.out.println("inside line");
-                        // DataTable chartdata = new DataTable();
-
-                        colum.add(new ColumnDescription(colHeader[0], ValueType.DATETIME, colHeader[0]));
-                        colum.add(new ColumnDescription(colHeader[1], ValueType.NUMBER, colHeader[1]));
-                        chartdata.addColumns(colum);
-                        String other = dt.toString();
+                else if (chartType.equals("line")) {
+                    GregorianCalendar calendar = new GregorianCalendar();
+                    calendar.setTimeZone(TimeZone.getTimeZone("GMT"));
+                    for (int i = 0; i < cDataList.size(); i++) {
+                        String other = cDataList.get(i).getName();
                         int year = Integer.parseInt(other.substring(0, 4));
                         int month = Integer.parseInt(other.substring(5, 7));
                         int date = Integer.parseInt(other.substring(8, 10));
                         int hours = Integer.parseInt(other.substring(11, 13));
                         int minutes = Integer.parseInt(other.substring(14, 16));
                         int seconds = Integer.parseInt(other.substring(17, 19));
-                        System.out.println("year\t" + year + "month\t" + month + "date\t" + date + "hours\t" + hours
-                                + "minutes\t" + minutes + "seconds\t" + seconds);
+                        // System.out.println("year\t" + year + "month\t" + month + "date\t" + date + "hours\t" +
+                        // hours
+                        // + "minutes\t" + minutes + "seconds\t" + seconds);
+
                         calendar.set(year, month, date, hours, minutes, seconds);
-                        for (int i = 0; i <= cDataList.size(); i++) {
-                            chartdata.addRowFromValues(cDataList.get(i).getName(), cDataList.get(i).getValue());
-                        }
-                        // datavalue.setChartData(JsonRenderer.renderDataTable(chartdata, true, true));
+                        chartdata.addRowFromValues(calendar, cDataList.get(i).getValue());
+                        System.out.println("line values::::\t" + chartdata.toString());
+
+                        // JSONObject json = new JSONObject();
+                        // json.put("date", cDataList.get(i).getName());
+                        // json.put("value", cDataList.get(i).getValue());
+                        // chartArray.put(json);
 
                     }
+                    // datavalue.setChartData(JsonRenderer.renderDataTable(chartdata, true, true));
+                }
+
+                if (chartType.equals("bar") || chartType.equals("pie")) {
                     renderchart = JsonRenderer.renderDataTable(chartdata, true, true);
-                    System.out.println("renderchart\t:::" + renderchart);
                     obj.setChartData(renderchart);
+                }
+                else if (chartType.equals("line")) {
+                    System.out.println("line values: before adding to obj:::\t" + chartArray.toString());
+                    renderchart = JsonRenderer.renderDataTable(chartdata, true, true);
+                    obj.setChartData(renderchart);
+                    // obj.setLineData(chartArray.toString());
+                    // System.out.println("getlinedata: \t" + obj.getLineData());
 
-                    // json.put("chart data", JsonRenderer.renderDataTable(chartdata, true, true));
+                }
+                else if (chartType.equals("plain")) {
+                    System.out.println("cDatalistsize:" + cDataList.size());
+                    System.out.println("cDatalistname:" + cDataList.get(0).getName());
+                    System.out.println("cDatalistvalue:" + cDataList.get(0).getValue());
+                    if (cDataList.size() >= 0)
+                        obj.setPlainData(cDataList.get(0).getValue());
+                    else
+                        obj.setPlainData(0);
+                    System.out.println("getplaindata: \t" + obj.getPlainData());
 
-                    // }
-                    datalist.add(datavalue);
+                }
 
-                }// end of while
-
-                obj.setData(datalist);
                 sqllist.add(obj);
-                // json.put("finallist", sqllist);
 
             }
 
@@ -268,9 +302,22 @@ public class DashboardServices {
                 JSONObject json = new JSONObject();
                 json.put("charttype", sqllist.get(i).getChartType());
                 json.put("divId", sqllist.get(i).getPositionId());
-                json.put("data", sqllist.get(i).getChartData());
+                if (sqllist.get(i).getChartType() == "plain") {
+                    json.put("data", sqllist.get(i).getPlainData());
+                    System.out.println("chart type in last for" + json.get("charttype") + "plaindaata inside json:::\t"
+                            + sqllist.get(i).getPlainData());
+                }
+                else if (sqllist.get(i).getChartType() == "line") {
+                    json.put("data", sqllist.get(i).getLineData());
+                    System.out.println("chart type in last for" + json.get("charttype") + "linedata inside json::::\t"
+                            + sqllist.get(i).getLineData());
+                }
+                else {
+                    json.put("data", sqllist.get(i).getChartData());
+                    System.out.println("chart type in last for" + json.get("charttype") + "chartdata inside json:::\t"
+                            + sqllist.get(i).getChartData());
+                }
                 chartArray.put(json);
-
                 // alist = gson.toJson(sqllist);
 
             }
@@ -278,14 +325,14 @@ public class DashboardServices {
         }
 
         catch (SQLException ex) {
-            System.out.println("exception occured" + ex);
+            // System.out.println("exception occured" + ex);
         }
 
         finally {
             // close connection ,stmt and resultset here
             dbCon.close();
             dbCon1.close();
-            System.out.println("connection state:\t" + dbCon.isClosed());
+            // System.out.println("connection state:\t" + dbCon.isClosed());
         }
         return Response.ok().entity(result).build();
 
