09da66f2faaba3e083bdef5cf80f20279894b0ef
rathish <rathish@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-08-19 12:15:28 +0000

    work in progress for  dynamic enterprise_id stuff

--- trunk/GQMapsCustomerServices/src/com/gq/meter/restsvcs/DashboardServices.java
+++ trunk/GQMapsCustomerServices/src/com/gq/meter/restsvcs/DashboardServices.java
@@ -10,9 +10,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -42,7 +44,9 @@ public class DashboardServices {
     @Path("/getdashboard")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getDashboardServices() throws ClassNotFoundException, SQLException, TypeMismatchException {
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getDashboardServices(@QueryParam("entpId") String entpId) throws ClassNotFoundException,
+            SQLException, TypeMismatchException {
 
         String dbURL = "jdbc:mysql://192.168.1.95:3306/gqexchange";
         String username = "gqmaps";
@@ -71,8 +75,9 @@ public class DashboardServices {
         String chartData = "";
         double plainData = 0;
         String lineData = "";
+        String plain = "";
         TaskAssist taskAssist = new TaskAssist(taskId, descr, sql, dynamic, chartType, columnHeader, relatedDb,
-                positionId, data, chartData, plainData, lineData);
+                positionId, data, chartData, plainData, lineData, plain);
         PreparedStatement pstmt = null;
         String alist = "";
         String result = "";
@@ -83,7 +88,7 @@ public class DashboardServices {
 
         CharSequence renderchart = null;
         TaskAssist jsonArrayList;
-
+        String dynamicChar = "";
         try {
             // getting database connection to MySQL server
             Class.forName("com.mysql.jdbc.Driver");
@@ -111,6 +116,7 @@ public class DashboardServices {
                 // stmt = (Statement) dbCon.prepareStatement(chartquery);
                 pstmt = (PreparedStatement) dbCon.prepareStatement(chartquery);
                 pstmt.setString(1, taskAssist.setChartType(rs.getString("ct_id")));
+                System.out.println("chart pstmt" + pstmt);
                 ResultSet chrttype = pstmt.executeQuery();
                 while (chrttype.next()) {
                     chartType = chrttype.getString("ct_id");
@@ -152,9 +158,43 @@ public class DashboardServices {
                 obj.setRelatedDb(relatedDb);
                 obj.setPositionId(positionId);
                 // sqllist.add(obj);
-
+                System.out.println("Dynamic value:" + dynamic);
+                // if (dynamic.equals("y")) {
+                // String entpquery = sql;
+                //
+                // System.out.println("dynamic queryis :\t" + entpquery + "entpid is::::\t" + entpId);
+                // dynamicChar = entpquery.replace("__filter", "?");
+                // String finalString = dynamicChar.replaceAll("[']", "");
+                // System.out.println("after replace:::::\t" + finalString);
+                //
+                // if (relatedDb.equalsIgnoreCase("m")) {
+                // // System.out.println("maps::::::::::");
+                //
+                // // System.out.println("query is \t" + entpquery);
+                // // stmt = (Statement) dbCon1.prepareStatement(entpquery);
+                // // // Resultset returned by query
+                // // entpResultset = stmt.executeQuery(entpquery);
+                //
+                // pstmt = (PreparedStatement) dbCon1.prepareStatement(finalString);
+                // System.out.println("before setstring:::");
+                // pstmt.setString(1, entpId);
+                // System.out.println("after setstring" + pstmt);
+                // entpResultset = pstmt.executeQuery();
+                // }
+                // else {
+                // // System.out.println("inside else");
+                // System.out.println("dynamic queryis exchange :\t" + entpquery + "entpid is::::\t" + entpId);
+                // pstmt = (PreparedStatement) dbCon.prepareStatement(finalString);
+                // System.out.println("before setstring:::");
+                // pstmt.setString(1, entpId);
+                // System.out.println("after setstring" + pstmt);
+                // entpResultset = pstmt.executeQuery();
+                // }
+                //
+                // }
+                // else {
                 if (relatedDb.equalsIgnoreCase("m")) {
-                    // System.out.println("maps::::::::::");
+                    System.out.println("inside non dynamic if::::::::::");
 
                     String entpquery = sql;
                     // System.out.println("query is \t" + entpquery);
@@ -163,13 +203,32 @@ public class DashboardServices {
                     entpResultset = stmt.executeQuery(entpquery);
                 }
                 else {
-                    // System.out.println("inside else");
+                    System.out.println("inside non dynamic else::::::::::");
                     String entpquery = sql;
                     stmt = (Statement) dbCon.prepareStatement(entpquery);
                     // Resultset returned by query
                     entpResultset = stmt.executeQuery(entpquery);
+                    // if (dynamic.equals("y")) {
+                    // System.out.println("dynamic queryis :\t" + entpquery + "entpid is::::\t" + entpId);
+                    // dynamicChar = entpquery.replace("__filter", "?");
+                    // String finalString = dynamicChar.replaceAll("[-+.^:']", "");
+                    // System.out.println("after replace:::::\t" + finalString);
+                    // pstmt = (PreparedStatement) dbCon1.prepareStatement(finalString);
+                    // System.out.println("before setstring:::");
+                    // pstmt.setString(1, entpId);
+                    // System.out.println("after setstring" + pstmt);
+                    // entpResultset = pstmt.executeQuery();
+                    // }
+                    // else {
+                    // System.out.println("after constructed" + pstmt);
+                    // stmt = (Statement) dbCon.prepareStatement(entpquery);
+                    // System.out.println("pstmt" + pstmt);
+                    // // Resultset returned by query
+                    // entpResultset = stmt.executeQuery(entpquery);
+                    // System.out.println("query executed");
+                    // }
                 }
-
+                // }
                 // find col count c =2;
                 ResultSetMetaData rsmd = entpResultset.getMetaData();
                 int count = rsmd.getColumnCount();
@@ -268,50 +327,75 @@ public class DashboardServices {
                     // datavalue.setChartData(JsonRenderer.renderDataTable(chartdata, true, true));
                 }
 
-                if (chartType.equals("bar") || chartType.equals("pie")) {
-                    renderchart = JsonRenderer.renderDataTable(chartdata, true, true);
-                    obj.setChartData(renderchart);
-                }
-                else if (chartType.equals("line")) {
-                    System.out.println("line values: before adding to obj:::\t" + chartArray.toString());
-                    renderchart = JsonRenderer.renderDataTable(chartdata, true, true);
-                    obj.setChartData(renderchart);
-                    // obj.setLineData(chartArray.toString());
-                    // System.out.println("getlinedata: \t" + obj.getLineData());
-
-                }
-                else if (chartType.equals("plain")) {
+                // if (chartType.equals("bar") || chartType.equals("pie")) {
+                // renderchart = JsonRenderer.renderDataTable(chartdata, true, true);
+                // obj.setChartData(renderchart);
+                // }
+                // else if (chartType.equals("line")) {
+                // System.out.println("line values: before adding to obj:::\t" + chartArray.toString());
+                // renderchart = JsonRenderer.renderDataTable(chartdata, true, true);
+                // obj.setChartData(renderchart);
+                // // obj.setLineData(chartArray.toString());
+                // // System.out.println("getlinedata: \t" + obj.getLineData());
+                // }
+                renderchart = JsonRenderer.renderDataTable(chartdata, true, true);
+                obj.setChartData(renderchart);
+                if (chartType.equals("plain")) {
                     System.out.println("cDatalistsize:" + cDataList.size());
                     System.out.println("cDatalistname:" + cDataList.get(0).getName());
                     System.out.println("cDatalistvalue:" + cDataList.get(0).getValue());
-                    if (cDataList.size() >= 0)
-                        obj.setPlainData(cDataList.get(0).getValue());
-                    else
+                    if (cDataList.size() > 0) {
+                        if (cDataList.get(0).getName() != null && cDataList.get(0).getName() != " ") {
+                            obj.setPlain(cDataList.get(0).getName());
+                            System.out.println("inside if");
+                            System.out.println("obj.getname::::::" + obj.getPlain());
+
+                        }
+                        else if (cDataList.get(0).getValue() != 0) {
+                            System.out.println("inside else if");
+                            obj.setPlainData(cDataList.get(0).getValue());
+                            System.out.println("obj.getvalue::::::" + obj.getPlainData());
+
+                        }
+                    }
+                    else {
                         obj.setPlainData(0);
+                        System.out.println("inside else");
+                    }
                     System.out.println("getplaindata: \t" + obj.getPlainData());
 
                 }
 
                 sqllist.add(obj);
-
             }
 
             JSONArray chartArray = new JSONArray();
             for (int i = 0; i < sqllist.size(); i++) {
+                System.out.println("plaiin sqllist data:::\t" + sqllist.get(i).getPlainData());
+
                 // jsonArrayList = sqllist.get(i);
                 JSONObject json = new JSONObject();
                 json.put("charttype", sqllist.get(i).getChartType());
                 json.put("divId", sqllist.get(i).getPositionId());
-                if (sqllist.get(i).getChartType() == "plain") {
-                    json.put("data", sqllist.get(i).getPlainData());
+                if (sqllist.get(i).getChartType().equals("plain")) {
                     System.out.println("chart type in last for" + json.get("charttype") + "plaindaata inside json:::\t"
                             + sqllist.get(i).getPlainData());
+                    if (sqllist.get(i).getPlain() != null && sqllist.get(i).getPlain() != " ") {
+                        json.put("data", sqllist.get(i).getPlain());
+                        System.out.println("chart type in last for" + json.get("charttype")
+                                + "plaindaata inside json:::\t" + sqllist.get(i).getPlain());
+                    }
+                    else if (sqllist.get(i).getPlainData() != 0) {
+                        json.put("data", sqllist.get(i).getPlainData());
+                        System.out.println("chart type in last for" + json.get("charttype")
+                                + "plaindaata inside json:::\t" + sqllist.get(i).getPlainData());
+                    }
                 }
-                else if (sqllist.get(i).getChartType() == "line") {
-                    json.put("data", sqllist.get(i).getLineData());
-                    System.out.println("chart type in last for" + json.get("charttype") + "linedata inside json::::\t"
-                            + sqllist.get(i).getLineData());
-                }
+                // else if (sqllist.get(i).getChartType().equals("line")) {
+                // json.put("data", sqllist.get(i).getLineData());
+                // System.out.println("chart type in last for" + json.get("charttype") + "linedata inside json::::\t"
+                // + sqllist.get(i).getLineData());
+                // }
                 else {
                     json.put("data", sqllist.get(i).getChartData());
                     System.out.println("chart type in last for" + json.get("charttype") + "chartdata inside json:::\t"
