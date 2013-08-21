4f32997d35e6ea082faa500a991a3692b24dc9c9
rathish <rathish@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-08-21 04:55:10 +0000

    mail redirected to support@gquotient.com

--- trunk/GQMapsRegistrationServices/src/com/gq/meter/model/EnterpriseModel.java
+++ trunk/GQMapsRegistrationServices/src/com/gq/meter/model/EnterpriseModel.java
@@ -7,10 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.Transport;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMultipart;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -34,7 +37,11 @@ public class EnterpriseModel {
      */
     Properties mailServerProperties;
     javax.mail.Session getMailSession;
-    MimeMessage generateMailMessage;
+    static javax.mail.Session generateMailMessage;
+
+    public static final String SMTP_OUT_SERVER = "smtpout.secureserver.net";
+    public static final String USER = "support@gquotient.com"; // godaddy domain
+    public static final String PASSWORD = "G&uot!3nt1";
 
     public boolean authenticate(Enterprise authObject) throws Exception {
         boolean authValue = false;
@@ -255,33 +262,41 @@ public class EnterpriseModel {
             List<Enterprise> entMeterResult = query.list();
             String tomail = String.valueOf(entMeterResult.get(0));
             System.out.println("mail id" + tomail);
-            System.out.println("\n 1st ===> setup Mail Server Properties..");
             mailServerProperties = System.getProperties();
-            mailServerProperties.put("mail.smtp.port", "587");
+            // step1
+            System.out.println("\n 1st ===> setup Mail Server Properties..");
+
+            mailServerProperties.setProperty("mail.transport.protocol", "smtp");
+            mailServerProperties.setProperty("mail.host", SMTP_OUT_SERVER);
             mailServerProperties.put("mail.smtp.auth", "true");
-            mailServerProperties.put("mail.smtp.starttls.enable", "true");
-            mailServerProperties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+            mailServerProperties.put("mail.smtp.port", "25");
+            mailServerProperties.setProperty("mail.user", USER);
+            mailServerProperties.setProperty("mail.password", PASSWORD);
             System.out.println("Mail Server Properties have been setup successfully..");
 
             // Step2
             System.out.println("\n\n 2nd ===> get Mail Session..");
-            getMailSession = javax.mail.Session.getDefaultInstance(mailServerProperties, null);
-            generateMailMessage = new MimeMessage(getMailSession);
-            generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(tomail));
-            generateMailMessage.addRecipient(Message.RecipientType.CC, new InternetAddress(
-                    "rathishyoungstani@gmail.com"));
-            generateMailMessage.setSubject("you are Registered Sucessfully");
-            String sb = "Dear Customer,<br><br> <p>Thank  you for registering for the gqmaps application.Your first step towards successfully managing and monitoring your IT infrastructure </p><p>Your registration process will be complete after manual verification of a few details by a GQuotient expert.</p><p>You will be contacted shortly at the number you have registered with to complete the process within the next 48 business hours.</p><p>Please do not reply to this mail.</p><p>If you are not contacted within the above time interval, please contact technical support at support@gquotient.com<br>Regards,<br><br>GQMaps Support.";
-            generateMailMessage.setContent(sb, "text/html");
-            System.out.println("Mail Session has been created successfully..");
+            generateMailMessage = javax.mail.Session.getDefaultInstance(mailServerProperties, null);
+            // generateMailMessage.setDebug(true);
+            Transport transport = generateMailMessage.getTransport("smtp");
+            MimeMessage message = new MimeMessage(generateMailMessage);
+            // message.setSentDate(new java.util.Date());
+            message.setSubject("you are Registered Sucessfully");
+            message.setFrom(new InternetAddress(USER));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(tomail));
+            message.setRecipient(Message.RecipientType.CC, new InternetAddress("support@gquotient.com"));
 
+            MimeMultipart multipart = new MimeMultipart("related");
+
+            BodyPart messageBodyPart = new MimeBodyPart();
+            String sb = "Dear Customer,<br><br> <p>Thank  you for registering for the gqmaps application.Your first step towards successfully managing and monitoring your IT infrastructure </p><p>Your registration process will be complete after manual verification of a few details by a GQuotient expert.</p><p>You will be contacted shortly at the number you have registered with to complete the process within the next 48 business hours.</p><p>Please do not reply to this mail.</p><p>If you are not contacted within the above time interval, please contact technical support at support@gquotient.com<br>Regards,<br><br>GQMaps Support.";
+            messageBodyPart.setContent(sb, "text/html");
+            multipart.addBodyPart(messageBodyPart);
+            message.setContent(multipart);
             // Step3
             System.out.println("\n\n 3rd ===> Get Session and Send mail");
-            Transport transport = getMailSession.getTransport("smtp");
-            // Enter your correct gmail UserID and Password
-            transport.connect("smtp.gmail.com", "rathishyoungstani@gmail.com", "rathish@8055");
-            transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
-            System.out.println("before closing the session");
+            transport.connect(SMTP_OUT_SERVER, USER, PASSWORD);
+            transport.sendMessage(message, message.getAllRecipients());
             transport.close();
             System.out.println("\n\n ===> Your Java Program has just sent an Email successfully. Check your email..");
             return entMeterResult;
@@ -329,37 +344,45 @@ public class EnterpriseModel {
             String tomail = String.valueOf(entpList.get(0).getEmail());
             String uName = String.valueOf(entpList.get(0).getUserId());
             String passwd = String.valueOf(entpList.get(0).getPasswd());
-            System.out.println("\n 1st ===> setup Mail Server Properties..");
             mailServerProperties = System.getProperties();
-            mailServerProperties.put("mail.smtp.port", "587");
+            // step1
+            System.out.println("\n 1st ===> setup Mail Server Properties..");
+
+            mailServerProperties.setProperty("mail.transport.protocol", "smtp");
+            mailServerProperties.setProperty("mail.host", SMTP_OUT_SERVER);
             mailServerProperties.put("mail.smtp.auth", "true");
-            mailServerProperties.put("mail.smtp.starttls.enable", "true");
-            mailServerProperties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+            mailServerProperties.put("mail.smtp.port", "25");
+            mailServerProperties.setProperty("mail.user", USER);
+            mailServerProperties.setProperty("mail.password", PASSWORD);
             System.out.println("Mail Server Properties have been setup successfully..");
 
             // Step2
             System.out.println("\n\n 2nd ===> get Mail Session..");
-            getMailSession = javax.mail.Session.getDefaultInstance(mailServerProperties, null);
-            generateMailMessage = new MimeMessage(getMailSession);
-            generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(tomail));
-            generateMailMessage.addRecipient(Message.RecipientType.CC, new InternetAddress(
-                    "rathishyoungstani@gmail.com"));
-            generateMailMessage.setSubject("you are Activated Sucessfully");
+            generateMailMessage = javax.mail.Session.getDefaultInstance(mailServerProperties, null);
+            // generateMailMessage.setDebug(true);
+            Transport transport = generateMailMessage.getTransport("smtp");
+            MimeMessage message = new MimeMessage(generateMailMessage);
+            // message.setSentDate(new java.util.Date());
+            message.setSubject("you are Registered Sucessfully");
+            message.setFrom(new InternetAddress(USER));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(tomail));
+            message.setRecipient(Message.RecipientType.CC, new InternetAddress("support@gquotient.com"));
+
+            MimeMultipart multipart = new MimeMultipart("related");
+
+            BodyPart messageBodyPart = new MimeBodyPart();
             String sb = "Dear Customer,<br><br><p>We are very pleased to welcome you to use the GQMaps application.The application can be accessed via http://<<192.168.1.95>>:8080/gqmaps</p> <br><br>User Id: "
                     + uName
                     + "<br>Password: "
                     + passwd
                     + "<br><br><p>Please change your password upon first login.Please do not reply to this mail.</p><p>If you have any difficulties accessing the application, technical support can be reached via support@gquotient.com</p><br>Regards,<br>GQMaps Support.";
-            generateMailMessage.setContent(sb, "text/html");
-            System.out.println("Mail Session has been created successfully..");
-
+            messageBodyPart.setContent(sb, "text/html");
+            multipart.addBodyPart(messageBodyPart);
+            message.setContent(multipart);
             // Step3
             System.out.println("\n\n 3rd ===> Get Session and Send mail");
-            Transport transport = getMailSession.getTransport("smtp");
-            // Enter your correct gmail UserID and Password
-            transport.connect("smtp.gmail.com", "rathishyoungstani@gmail.com", "rathish@8055");
-            transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
-            System.out.println("before closing the session");
+            transport.connect(SMTP_OUT_SERVER, USER, PASSWORD);
+            transport.sendMessage(message, message.getAllRecipients());
             transport.close();
             System.out.println("\n\n ===> Your Java Program has just sent an Email successfully. Check your email..");
             return entpList;
