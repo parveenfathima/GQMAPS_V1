cfd3214dcc6c44985d487a63955345c6012f8ee8
rathish <rathish@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-08-29 13:08:03 +0000

    Updated all the services with loggers and minor changes and removed the junk class files

--- trunk/GQMapsCustomerServices/src/com/gq/meter/model/MapsDomainModel.java
+++ trunk/GQMapsCustomerServices/src/com/gq/meter/model/MapsDomainModel.java
@@ -1,6 +1,5 @@
 package com.gq.meter.model;
 
-import java.util.Iterator;
 import java.util.List;
 
 import org.hibernate.Query;
@@ -11,7 +10,7 @@ import com.gq.meter.object.DevCtlg;
 import com.gq.meter.object.SrvrAppType;
 import com.gq.meter.object.CompType;
 import com.gq.meter.object.AssetImp;
-import com.gq.meter.util.MapsDomainServiceConstant;
+import com.gq.meter.util.CustomerServiceConstant;
 import com.gq.meter.util.HibernateUtil;
 
 public class MapsDomainModel {
@@ -20,31 +19,31 @@ public class MapsDomainModel {
         Session session = null;
         MapsDomainBean mapsDomainServices = null;
         try {
-
             session = HibernateUtil.getSessionFactory().getCurrentSession();
             session.beginTransaction();
-
             String devCtlg = "FROM DevCtlg";
             Query devCtlgQuery = session.createQuery(devCtlg);
             List<DevCtlg> devCtlgResult = devCtlgQuery.list();
-
+            CustomerServiceConstant.logger
+                    .info("[MAPSDOMAINMODEL]  MapsDomain is Sucessfully Executed the DeviceCatalogue");
             String srvrAppType = "FROM SrvrAppType";
             Query srvrAppTypeQuery = session.createQuery(srvrAppType);
             List<SrvrAppType> srvrAppTypeResult = srvrAppTypeQuery.list();
-
+            CustomerServiceConstant.logger
+                    .info("[MAPSDOMAINMODEL]  MapsDomain is Sucessfully Executed the Server AppType");
             String compType = "FROM CompType";
             Query compTypeQuery = session.createQuery(compType);
             List<CompType> compTypeResult = compTypeQuery.list();
-
+            CustomerServiceConstant.logger.info("[MAPSDOMAINMODEL]  MapsDomain is Sucessfully Executed the Comp Type");
             String assetImp = "FROM AssetImp";
             Query assetImpQuery = session.createQuery(assetImp);
             List<AssetImp> assetImpResult = assetImpQuery.list();
-
+            CustomerServiceConstant.logger.info("[MAPSDOMAINMODEL]  MapsDomain is Sucessfully Executed the Asset Impl");
             mapsDomainServices = new MapsDomainBean(devCtlgResult, srvrAppTypeResult, compTypeResult, assetImpResult);
-
         }
         catch (Exception e) {
-            MapsDomainServiceConstant.logger.error("Exception occured while fetching the CustomerServiceDetails ", e);
+            CustomerServiceConstant.logger.error(
+                    "[MAPSDOMAINMODEL]  Exception occured while fetching the CustomerServiceDetails ", e);
         }
         finally {
             try {
@@ -57,6 +56,7 @@ public class MapsDomainModel {
                 e.printStackTrace();
             }
         }
+        CustomerServiceConstant.logger.info("[MAPSDOMAINMODEL]  MapsDomain is Sucessfully Executed");
         return mapsDomainServices;
     }
 }
