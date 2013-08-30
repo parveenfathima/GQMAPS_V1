30c75b9de63e03dee5ddff4c3b4b65b03c5b1ce0
hema@itechins.com <hema@itechins.com@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-08-30 08:51:55 +0000

    updated - Separated UIs

--- trunk/GQMapsRegistrationUI/WebContent/js/rest_service.js
+++ trunk/GQMapsRegistrationUI/WebContent/js/rest_service.js
@@ -1,9 +1,7 @@
 $(document).ready(function() 
 {
-	//$.jStorage.set("jsUrl", "http://192.168.1.95:8080/GQMapsRegistrationServices/gqm-gk/");
-	//$.jStorage.set("jsDBUrl", "http://192.168.1.95:8080/GQMapsCustomerServices/gqm-gqedp/");
-	$.jStorage.set("jsUrl", "http://localhost:8080/GQMapsRegistrationServices/gqm-gk/");
-	$.jStorage.set("jsDBUrl", "http://localhost:8080/GQMapsCustomerServices/gqm-gqedp/");
+	$.jStorage.set("jsUrl", "http://192.168.1.95:8080/GQMapsRegistrationServices/gqm-gk/");
+	$.jStorage.set("jsDBUrl", "http://192.168.1.95:8080/GQMapsCustomerServices/gqm-gqedp/");
 });
 
 function logout()
@@ -11,11 +9,15 @@ function logout()
 	$.jStorage.flush();
 }
 
-//http://localhost:8080/GQMapsCustomerServices/gqm-gqedp/DashboardServices/getdashboard
+/*
 
+=================================================================================================================================================================	
+asset_edit.html services
 
-//http://localhost:8080/GQMapsCustomerServices1/gqm-gqedp/getAssetServices/getAssetData
-/*http://localhost:8080/GQMapsCustomerServices1/gqm-gqedp/updateAssetServices/updateAssetData
+http://localhost:8080/GQMapsCustomerServices/gqm-gqedp/mapsDomainServices/getMapsDomainData
+http://localhost:8080/GQMapsCustomerServices/gqm-gqedp/getAssetServices/getAssetData
+
+http://localhost:8080/GQMapsCustomerServices1/gqm-gqedp/updateAssetServices/updateAssetData
 {
             "assetId": "C-8c89a539be66",
             "name": "GQCHNSW01",
@@ -33,14 +35,47 @@ function logout()
             "inactiveDttm": "2013-08-06 12:56:09",
             "typeId": "desktop"
             }
-*/
-
-//http://localhost:8080/GQMapsCustomerServices1/gqm-gqedp/mapsDomainServices/getMapsDomainData
-
-
-
-/*
 
 				var jsonLineData = {"cols":[{"id":"DateTime","label":"col0","type":"datetime","pattern":""},{"id":"CPULoad","label":"col1","type":"number","pattern":""}],"rows":[{"c":[{"v":new Date(2013,8,1,10,4,50)},{"v":2.0}]},{"c":[{"v":new Date(2013,8,1,10,56,57)},{"v":6.0}]},{"c":[{"v":new Date(2013,8,1,11,0,24)},{"v":3.0}]},{"c":[{"v":new Date(2013,8,1,11,3,25)},{"v":3.0}]},{"c":[{"v":new Date(2013,8,1,12,32,0)},{"v":1.0}]},{"c":[{"v":new Date(2013,8,1,12,32,50)},{"v":1.0}]},{"c":[{"v":new Date(2013,8,1,13,44,33)},{"v":2.0}]},{"c":[{"v":new Date(2013,8,1,14,6,52)},{"v":3.0}]},{"c":[{"v":new Date(2013,8,1,14,9,43)},{"v":10.0}]}]}	
+=================================================================================================================================================================				
+
+login.html services
+
+GET
+enterprise/getRegistration
+
+=================================================================================================================================================================				
+				
+edit_registration.html services
+
+GET
+enterprise/updateRegistration
+enterpriseMeters/addEntMeters
+gatekeeper/addEntAudit
+general/getEntpSummaryList
+enterpriseMeters/getEntMeters
+"enterpriseMeters/getEntMeters/?entpId=" + arrEntp[i].getEId();
+enterprise/getRegistration				
+
+
+POST
+enterpriseMeters/addEntMeters
+gatekeeper/addEntAudit
+
+
+PUT
+enterprise/updateRegistration
+					
+=================================================================================================================================================================				
+				
+dashboard_full.html services
+
+http://localhost:8080/GQMapsCustomerServices/gqm-gqedp/DashboardServices/getdashboard
+=================================================================================================================================================================					
+				
+				
+				
+				
+				
 				
 				*/
