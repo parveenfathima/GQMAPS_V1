508f48cbaae9c25e9b35b3dd2b1b9b0a7fc55e90
parveenf <parveenf@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-09-11 06:52:02 +0000

    Changed log information and added comment Lines

--- trunk/GQMapsCustomerServices/src/com/gq/meter/restsvcs/AssetEditServices.java
+++ trunk/GQMapsCustomerServices/src/com/gq/meter/restsvcs/AssetEditServices.java
@@ -17,24 +17,32 @@ import com.gq.meter.object.Asset;
 
 import com.gq.meter.util.CustomerServiceConstant;
 
+/**
+ * @author parveen
+ * 
+ */
+
+// This class is define the services of getting and updating asset(computer,printer,nsrg and storage) data's
 @Path("/AssetEditServices")
 public class AssetEditServices {
+
     @Path("/getAssetData")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    // This method is takes care of getting asset details for requesting enterprise Id.
     public String getAllAssetDetails(@QueryParam("enterpriseId") String enterpriseId) {
-        System.out.println(enterpriseId);
-        CustomerServiceConstant.logger
-                .info(" [AssetEditServices]  Generating all the Get Asset service list from GQEntrprseDataProcessor");
+
+        CustomerServiceConstant.logger.info("Generating all the Get Asset service list from GQEntrprseDataProcessor");
         AssetEditModel getAssetModel = new AssetEditModel();
         GetAsset getAssetResult = getAssetModel.getAssetDetails(enterpriseId);
-        // Returning all the enterprises in JSON format
+        // Returning all the Assets in JSON format
         return CustomerServiceConstant.gson.toJson(getAssetResult);
     }
 
     @Path("/updateAssetData")
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
+    // This method is takes care of updating asset details for requesting enterprise Id.
     public Response updateAsset(@QueryParam("enterpriseId") String enterpriseId, String assetObjectString) {
         Asset[] assetObject = null;
         try {
@@ -46,11 +54,9 @@ public class AssetEditServices {
             }
         }
         catch (Exception e) {
-            CustomerServiceConstant.logger
-                    .error(" [AssetEditServices]  Exception occured while updating enterprise", e);
+            CustomerServiceConstant.logger.error("Exception occured while updating enterprise", e);
             return Response.status(400).build();
         }
-        // return Response.status(200).build();
         return Response.ok(CustomerServiceConstant.gson.toJson("success")).build();
     }
 }
--- trunk/GQMapsCustomerServices/src/com/gq/meter/restsvcs/MapsDomainServices.java
+++ trunk/GQMapsCustomerServices/src/com/gq/meter/restsvcs/MapsDomainServices.java
@@ -4,25 +4,33 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+
 import javax.ws.rs.core.MediaType;
 
 import com.gq.meter.model.MapsDomainModel;
+
 import com.gq.meter.object.MapsDomainBean;
+
 import com.gq.meter.util.CustomerServiceConstant;
 
+/**
+ * @author parveen
+ * 
+ */
+// This class is define the services of all domain data's
 @Path("/mapsDomainServices")
 public class MapsDomainServices {
 
     @Path("/getMapsDomainData")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getAllCatalogDetails(@QueryParam("enterpriseId") String enterpriseId) {
-        CustomerServiceConstant.logger.info(" [MapsDomainServices]  Generating all Domain service list from gqm"
-                + enterpriseId);
+    // This method takes care of getting all domain details
+    public String getAllDomainDetails(@QueryParam("enterpriseId") String enterpriseId) {
+
+        CustomerServiceConstant.logger.info("Generating all Domain service list from gqm" + enterpriseId);
         MapsDomainModel mapsDomainModel = new MapsDomainModel();
         MapsDomainBean mapsDomainResult = mapsDomainModel.getAllMapsDomainDetails(enterpriseId);
-        // Returning all the enterprises in JSON format
+        // Returning all the domain list in JSON format
         return CustomerServiceConstant.gson.toJson(mapsDomainResult);
-        // return mapsDomainResult;
     }
 }
