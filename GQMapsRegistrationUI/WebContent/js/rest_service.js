718c240e222dc070db27208a55a9c464bfc0e28d
hema <hema@itechins.com>
2013-09-17 05:56:10 +0000

    updated

--- GQMapsRegistrationUI/WebContent/js/rest_service.js
+++ GQMapsRegistrationUI/WebContent/js/rest_service.js
@@ -1,9 +1,9 @@
 $(document).ready(function() 
 {
-	//$.jStorage.set("jsUrl", "http://192.168.1.95:8080/GQMapsRegistrationServices/");
-	//$.jStorage.set("jsDBUrl", "http://192.168.1.95:8080/GQMapsCustomerServices/");
-	$.jStorage.set("jsUrl", "http://localhost:8080/GQMapsRegistrationServices/");
-	$.jStorage.set("jsDBUrl", "http://localhost:8080/GQMapsCustomerServices/");
+	$.jStorage.set("jsUrl", "http://192.168.1.95:8080/GQMapsRegistrationServices/");
+	$.jStorage.set("jsDBUrl", "http://192.168.1.95:8080/GQMapsCustomerServices/");
+	//$.jStorage.set("jsUrl", "http://localhost:8080/GQMapsRegistrationServices/");
+	//$.jStorage.set("jsDBUrl", "http://localhost:8080/GQMapsCustomerServices/");
 });
 
 function logout()
