addf27716ab9ed54734e282ef70849fb175088d4
hema <hema@itechins.com>
2013-09-20 11:21:18 +0000

    Bug 57 - changes in the overall screen

--- GQMapsCustomerUI/WebContent/js/add_registration.js
+++ GQMapsCustomerUI/WebContent/js/add_registration.js
@@ -4,13 +4,12 @@ $(document).ready(function()
 {
 		$("#submit").bind("click", validateForm);
 	
-		// called when key is pressed in txtPhone textbox
 		$("#txtPhone, #txtESqft, #txtEAsset, #txtDCSqft, #txtDCAsset, #txtDCUsed, #txtDCTemp").keypress(function (e) {
 		// if the letter is not digit then display error and don't type anything
 		 if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) 
 		 {
 			//display error message
-			alert("Enter numbers only");
+			alert("Please enter numbers only");
 			return false;
 		 }
 	   }); 
