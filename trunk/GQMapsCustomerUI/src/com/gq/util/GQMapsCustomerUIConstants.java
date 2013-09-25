03250cf446fa5dcc65cddbad5e1fd47352727375
hema@itechins.com <hema@itechins.com@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-09-25 06:54:45 +0000

    logger implementation for customer ui

---
+++ trunk/GQMapsCustomerUI/src/com/gq/util/GQMapsCustomerUIConstants.java
@@ -0,0 +1,39 @@
+package com.gq.util;
+
+
+import java.io.IOException;
+import java.util.Date;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
+public class GQMapsCustomerUIConstants {
+    // logger specifics
+    public static final Logger logger = Logger.getLogger("GQMapsCustomerConstants.class");
+
+    private static FileAppender fileappender;
+    private static String logFilePrefix = "/opt/gq/maps/logs/";
+    private static String logFileName = "enterpriseDataProcessor.log";
+
+    private static final String LOG_FILE_NAME_DATE_PATTERN = "yyyy-MM-dd";
+
+    static {
+        logger.info("Inside create logger method");
+        GQMapsCustomerUIConstants.logger.info("initializing gqmaps EntrprseDataProcessor logger..."); 
+        String logFile = logFilePrefix + logFileName;
+        try {
+            fileappender = new org.apache.log4j.DailyRollingFileAppender(new PatternLayout(
+                    "  %-15C{1} | %d{MMM dd HH:mm} | %5p | %m%n"), logFile, LOG_FILE_NAME_DATE_PATTERN);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            GQMapsCustomerUIConstants.logger.error("************ Unable to create LOG file handle for file  " + logFile + " at "
+                    + new Date() + ". TERMINATING server ??.... ************"); 
+        }
+        logger.addAppender(fileappender);
+        logger.info("after create logger method");
+    }
+
+}
+
+// GQEDPConstants.logger.info(" ....");
