0c95e2f583c1fae712f5df4afb81e8c594f5c0e8
rathish <rathish@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-10-10 10:25:18 +0000

    new object added updated previous objects

---
+++ GQMeter-Objects/src/com/gq/meter/xchange/object/TemplateTaskDetails.java
@@ -0,0 +1,73 @@
+package com.gq.meter.xchange.object;
+
+import java.sql.Timestamp;
+
+public class TemplateTaskDetails {
+
+    private int snpsht_id;
+    private Timestamp apply_date;
+    private int cost_benefit;
+    private String usr_notes;
+    private String sys_notes;
+    private int task_id;
+    private String descr;
+    private int ts_id;
+    private String tooltip;
+    private String chartData;
+
+    public TemplateTaskDetails(int snpsht_id, Timestamp apply_date, int cost_benefit, String usr_notes,
+            String sys_notes, int task_id, String descr, int ts_id, String tooltip, String chartData) {
+        super();
+        this.snpsht_id = snpsht_id;
+        this.apply_date = apply_date;
+        this.cost_benefit = cost_benefit;
+        this.usr_notes = usr_notes;
+        this.sys_notes = sys_notes;
+        this.task_id = task_id;
+        this.descr = descr;
+        this.ts_id = ts_id;
+        this.tooltip = tooltip;
+        this.chartData = chartData;
+    }
+
+    public String getChartData() {
+        return chartData;
+    }
+
+    public int getSnpsht_id() {
+        return snpsht_id;
+    }
+
+    public Timestamp getApply_date() {
+        return apply_date;
+    }
+
+    public int getCost_benefit() {
+        return cost_benefit;
+    }
+
+    public String getUsr_notes() {
+        return usr_notes;
+    }
+
+    public String getSys_notes() {
+        return sys_notes;
+    }
+
+    public int getTask_id() {
+        return task_id;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public int getTs_id() {
+        return ts_id;
+    }
+
+    public String getTooltip() {
+        return tooltip;
+    }
+
+}
