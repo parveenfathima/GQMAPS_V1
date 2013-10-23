0b34158c8894c4070057efeb5dfcb6c1b94d7209
mistersriram <mistersriram@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-10-23 07:17:35 +0000

    bug fix

--- GQMapsCustomerServices/src/com/gq/meter/util/DynamicSessionUtil.java
+++ GQMapsCustomerServices/src/com/gq/meter/util/DynamicSessionUtil.java
@@ -2,6 +2,7 @@ package com.gq.meter.util;
 
 import java.util.HashMap;
 
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -13,32 +14,50 @@ public class DynamicSessionUtil {
 
     private static HashMap<String, SessionFactory> SessionFactoryListMap = new HashMap<String, SessionFactory>(10);
 
-    public static SessionFactory getSessionFactory(String dbInstanceName) {
+    public static synchronized SessionFactory getSessionFactory(String dbInstanceName) {
 
         SessionFactory sessionFactory = null;
         String url = "jdbc:mysql://localhost:3306/" + dbInstanceName;
-        // String url = "jdbc:mysql://192.168.8.15:3306/" + dbInstanceName;
 
         if (SessionFactoryListMap.containsKey(dbInstanceName)) {
+        	CustomerServiceConstant.logger.info("Session Factory exists for " + dbInstanceName);
             sessionFactory = SessionFactoryListMap.get(dbInstanceName);
+            try {
+                if ((sessionFactory == null) || (sessionFactory.getCurrentSession() == null)) {
+                	CustomerServiceConstant.logger.debug("Null session factory so newly creating one for .." + dbInstanceName);
+                    createSessionFactory(dbInstanceName, url);
+                }
+            }
+            catch(HibernateException he){
+            	he.printStackTrace();
+            	CustomerServiceConstant.logger.debug("HibernateException so newly creating one for .." + dbInstanceName);
 
-            if ((sessionFactory == null) || (sessionFactory.getCurrentSession() == null)) {
-                createSessionFactory(dbInstanceName, url);
+                try {
+                	createSessionFactory(dbInstanceName, url);
+                }
+                catch(HibernateException he1){
+                	he.printStackTrace();
+                	CustomerServiceConstant.logger.debug("HibernateException createSessionFactory .." + dbInstanceName);
+                }
+
             }
         }
         else {
+        	CustomerServiceConstant.logger.debug("No entry in map yet for .." + dbInstanceName + " , creating one ...");
             createSessionFactory(dbInstanceName, url);
         }
+        
         return SessionFactoryListMap.get(dbInstanceName);
     } // method end
 
     private static void createSessionFactory(String dbInstanceName, String url) {
 
-        Configuration configuration = new Configuration();
-        configuration.setProperty("hibernate.connection.url", url);
-        configuration.configure();
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
-        SessionFactoryListMap.put(dbInstanceName, sessionFactory);
-    }
+		    Configuration configuration = new Configuration();
+		    configuration.setProperty("hibernate.connection.url", url);
+		    configuration.configure();
+		    SessionFactory sessionFactory = configuration.buildSessionFactory();
+		    CustomerServiceConstant.logger.debug("Session Factory created");
+		    SessionFactoryListMap.put(dbInstanceName, sessionFactory);
+    } // method ends
 
 }
