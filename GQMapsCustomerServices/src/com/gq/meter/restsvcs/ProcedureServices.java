1bb3e9b9e1a97773b2c25c907eb376949b9c9ae8
parveenf <parveenf@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-10-25 10:26:39 +0000

    Removed unnecessary spaces and console lines

--- GQMapsCustomerServices/src/com/gq/meter/restsvcs/ProcedureServices.java
+++ GQMapsCustomerServices/src/com/gq/meter/restsvcs/ProcedureServices.java
@@ -22,8 +22,8 @@ import com.mysql.jdbc.CallableStatement;
 import com.mysql.jdbc.Statement;
 
 /**
- * @author GQ
- * 
+ * @author rathish
+ * @change parveen
  */
 @Path("/procedure")
 // no generic code as of now have to write in future
@@ -39,11 +39,13 @@ public class ProcedureServices {
         Connection dbExchange = null;
         CallableStatement pstmt = null;
         Statement stmt = null;
+
         double output = 0;
         String result = "";
         String tsql = "";
         ResultSet rs = null;
         double value = 0;
+
         try {
             // getting database connection to MySQL server
             dbExchange = SqlUtil.getExchangeConnection();
@@ -53,21 +55,24 @@ public class ProcedureServices {
             // Resultset returned by query
             rs = stmt.executeQuery(sql);
             CustomerServiceConstant.logger.info(" Query sucessfully Executed from the TaskAsst Table");
+
             while (rs.next()) {
                 tsql = rs.getString("tsql");
             }
+
             String charreplace = tsql.replace("__string_in__", "?");
             String finalquery = charreplace.replace("__double_out__", "?");
             pstmt = (CallableStatement) dbCustomer.prepareCall(finalquery);
             pstmt.setString(1, "all");
             pstmt.registerOutParameter(2, Types.DOUBLE);
             rs = pstmt.executeQuery();
+
             if (rs.next()) {
                 output = rs.getDouble(1);
                 value = Math.round(output * 100.0) / 100.0;
             }
             else {
-                System.out.println("nodata");
+                CustomerServiceConstant.logger.info(" nodata ");
             }
             CustomerServiceConstant.logger.error(" PUE data" + output);
             result = Double.toString(value);
@@ -79,6 +84,5 @@ public class ProcedureServices {
             CustomerServiceConstant.logger.error(" Exception Occured during the Connection" + e);
         }
         return Response.ok(result).build();
-
     }
 }
