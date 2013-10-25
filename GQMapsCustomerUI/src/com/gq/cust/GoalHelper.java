539b0c12de8ede74fac1ced01cfe3a855aa2717a
kiruba.k <kiruba.k@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-10-25 11:29:47 +0000

    initial major revision for goal related services and dashboard changes

--- GQMapsCustomerUI/src/com/gq/cust/GoalHelper.java
+++ GQMapsCustomerUI/src/com/gq/cust/GoalHelper.java
@@ -1,17 +1,17 @@
 package com.gq.cust;
 
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 import com.google.gson.*;
-import com.gq.meter.object.Asset;
-import com.gq.meter.object.DevCtlg;
+
 import com.gq.meter.xchange.object.Goal;
 import com.gq.meter.xchange.object.GoalMaster;
 import com.gq.meter.xchange.object.GoalSnpsht;
-import com.gq.meter.xchange.object.TaskTmplt;
+import com.gq.meter.xchange.object.TemplateTaskDetails;
+import com.gq.util.GQMapsCustomerUIConstants;
+
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
@@ -19,24 +19,55 @@ import com.sun.jersey.api.client.WebResource;
 public class GoalHelper {
 
 	GoalMaster gm;
-	static Gson gson = new GsonBuilder().setDateFormat("yyyy-mm-dd hh:mm:ss")
-			.create();
+	
+	static Gson gson = new GsonBuilder().setDateFormat("yyyy-mm-dd hh:mm:ss").create();
 
-	public GoalHelper(String enterpriseId , String goalId) {
-
+	public GoalHelper(String enterpriseId , String goalId, String goalInputs) {
+		
 		Client client = Client.create();
+		WebResource service;
+		ClientResponse response;
+		
+		service = client.resource("http://"+GQMapsCustomerUIConstants.webSvcHost+":8080/GQMapsCustomerServices/");
 
-		WebResource webResourceAsset = client
-				.resource("http://192.168.8.15:8080/GQMapsCustomerServices/goalServices/goal?goalId="
-							+ goalId + "&entpId=" + enterpriseId);
+		if ( goalInputs != null ) { // for goals which dont have no goal inputs set
+			goalInputs = goalInputs + "~__ent_id__=" + enterpriseId;
+		}
+		else {
+			goalInputs = "__ent_id__=" + enterpriseId;
+		} 
+		
+		response = service.path("goalServices").path("goal")
+								.queryParam("goalId",goalId)
+								.queryParam("entpId", enterpriseId)
+								.queryParam("goalInputs",goalInputs)
+								.get(ClientResponse.class);
+		
+		System.out.println("ks enterpriseId = <" +enterpriseId+ "> , uri = <"+response+">");
+		
+		if (response.getStatus() != 200) {
+			   throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+		}
+	 
+		String svcOutput = response.getEntity(String.class);
+		System.out.println("kb jsaon resp:::"+svcOutput);
+		
+		gm = gson.fromJson(svcOutput , GoalMaster.class);
+		
+		//------------------
+//		Client client = Client.create();
+//
+//		WebResource webResourceAsset = client
+//				.resource("http://"+GQMapsCustomerUIConstants.webSvcHost+":8080/GQMapsCustomerServices/goalServices/goal?goalId="
+//							+ goalId + "&entpId=" + enterpriseId+"&goalInputs=__ent__=" + enterpriseId );
+//
+//		//ClientResponse response = webResourceAsset.accept("application/json").get(ClientResponse.class);
+//
+//		String jsonString = webResourceAsset.get(String.class);
 
-		ClientResponse response = webResourceAsset.accept("application/json").get(ClientResponse.class);
+		//System.out.println("AssetData String: " + jsonString);
 
-		String jsonString = webResourceAsset.get(String.class);
-
-		System.out.println("AssetData String: " + jsonString);
-
-		gm = gson.fromJson(jsonString, GoalMaster.class);
+//		gm = gson.fromJson(response. , GoalMaster.class);
 
 	}
 
@@ -51,12 +82,22 @@ public class GoalHelper {
 		return gm.getGoal();
 	}
 
-	// method ends
-
-	public List<TaskTmplt> getTaskTmplt() throws NoSuchElementException,
-			ParseException {
-
-		return gm.getTaskList();
+	public List<TemplateTaskDetails> getTemplateTaskDetails() throws NoSuchElementException,
+	ParseException {
+//		System.out.println("num gcd records = "+gm.getTemplateTaskDetails().size());
+//		
+//		for (int i = 0; i < gm.getTemplateTaskDetails().size(); i++) {
+//			
+//			System.out.println("gcd.java.type = "+gm.getTemplateTaskDetails().get(i).getChartType());
+//			if ( gm.getTemplateTaskDetails().get(i).getChartType().equals("line")) {
+//				System.out.println("gcd.java for line before = "+gm.getTemplateTaskDetails().get(i).getChartData());
+//				
+//				System.out.println("gcd.java for line after = "+gm.getTemplateTaskDetails().get(i).getChartData().replaceFirst("string", "datetime"));
+//			}
+//		}
+		
+		return gm.getTemplateTaskDetails();
 	}
+
 
 }
