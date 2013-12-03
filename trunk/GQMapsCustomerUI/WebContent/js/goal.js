361f170b4ce58959c5da184e32ef8206c969addc
parveenf <parveenf@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-12-03 07:31:02 +0000

    Removed unwanted alerts and Fixed Login,goal screen bugs

--- trunk/GQMapsCustomerUI/WebContent/js/goal.js
+++ trunk/GQMapsCustomerUI/WebContent/js/goal.js
@@ -9,21 +9,14 @@ var barChartOptions = {'title':'',
    'width':300,
    'height':200};
 
-
 function showGoalGraphs(){
-	//alert("inside show goal graph function , # of tasks = " + document.getElementById('taskCount').value);
 
 	for (var j=0 ; j < document.getElementById('taskCount').value;j++){
 
 		var cdData = 'chartData_'+ j;
 		var cdDiv = 'chartDiv_'+ j;
 		var cdType = 'chartType_'+ j;
-		
-//		alert( 'chartData_'+ j+ ' = ' + document.getElementById(cdData).value
-//			+ '  ,  chartDiv_'+ j+ ' = ' + document.getElementById(cdDiv).id
-//			+ '  ,  chartType_'+ j+ ' = <' + document.getElementById(cdType).value +'>'
-//		);
-		
+				
 		if (document.getElementById(cdType).value == "pie"){
 			showGoalPieChart( document.getElementById(cdData).value  , document.getElementById(cdDiv).id );
 		}
@@ -45,20 +38,14 @@ function showGoalGraphs(){
 
 function showGoalBarChart(barData, divId)
 {
-	//alert("inside bar chart");
-	var jsarray = JSON.parse(barData);		
-	//alert("jsarray" + jsarray);
+		var jsarray = JSON.parse(barData);		
 		var data = new google.visualization.DataTable(jsarray);
-	//alert("data" + data);
 		var chart = new google.visualization.BarChart(document.getElementById(divId));
-		chart.draw(data, barChartOptions);
-	
+		chart.draw(data, barChartOptions);	
 }
-
 
 function showGoalPieChart(pieData,divId)
 {	 
-	//alert("inside pie chart");
 	var jsarray = JSON.parse(pieData);
 	var data = new google.visualization.DataTable( jsarray );
 	
@@ -67,7 +54,6 @@ function showGoalPieChart(pieData,divId)
 }
 
 function dateReviver(key, value) {
-	//alert("dateReviver key = "+ key +" , val = "+ value);
     var a;
     if (typeof value === 'string') {
     	
@@ -84,27 +70,13 @@ function dateReviver(key, value) {
 
 function showGoalATLChart(lineData, divId){	
 	
-//	alert(" Line data " + lineData );
-	
-//	if(lineData.charAt(0) === '"')
-//		lineData = lineData.substr(1);
-	
-//	alert(" Line data 2 " + lineData );
-	
 	lineData = lineData.substr(1,lineData.length-2);
-//	alert(" Line data 3 " + lineData );
-
 	lineData = lineData.replace(/\\/g,"");
-//	alert('string sl c re = ' + lineData );
+	
 	var jsarray = JSON.parse(lineData,dateReviver);
-	
-//	alert("showGoalATLChart "+ JSON.stringify(jsarray));
-	
 	var data = new google.visualization.DataTable(jsarray);
-//	alert("showGoalATLChart 2 data " + data );
-	
 	var annotatedtimeline = new google.visualization.AnnotatedTimeLine(document.getElementById(divId));
-		//alert("data" + annotatedtimeline);
+	
 	annotatedtimeline.draw(data, {'displayAnnotations': true,
 		lineWidth: 1,
 		'fill': 0,
@@ -117,14 +89,17 @@ function showGoalATLChart(lineData, divId){
 		'colors': ['green', 'red', 'orange'],
 		'displayRangeSelector' : false
 		});
-	
-
 }
 
 //function to display text in the specified div location
 function showGoalPlainChart(plainText, divId)
 {
-	//alert( 'showPlainChart , chartData = ' + plainText 	+ '  ,  chartDiv_ = ' + divId );
-	document.getElementById(divId).innerHTML = plainText;
-	 
+	document.getElementById(divId).innerHTML = plainText;	 
 }
+
+//function to redirect Dashboard screen    
+function gotoDashboard() {
+
+	window.location.href="dashboard_full.html";
+
+		}
