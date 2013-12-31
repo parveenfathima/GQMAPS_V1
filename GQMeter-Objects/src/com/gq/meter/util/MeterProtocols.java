79658495d61edfbe82e8e6c92c98084c4673ce3f
parveenf <parveenf@82efd7aa-5ada-47e4-9ce6-1c3ee3813419>
2013-12-31 10:23:02 +0000

    New switch added

--- GQMeter-Objects/src/com/gq/meter/util/MeterProtocols.java
+++ GQMeter-Objects/src/com/gq/meter/util/MeterProtocols.java
@@ -2,5 +2,5 @@ package com.gq.meter.util;
 
 public enum MeterProtocols {
 
-    PRINTER, NSRG, COMPUTER, STORAGE, POWER, AIR, WATER, UNKNOWN
+    PRINTER, NSRG, COMPUTER, STORAGE, POWER, AIR, WATER, UNKNOWN, SPEEDTEST
 }
